7803875684fe5ce3fd5fff476dd4d5cfdd979385
Danish Munir <dtmunir@gmail.com>
2015-03-05 16:33:35 -0500

    Flush out conversations api

--- app/api/leo/api.rb
+++ app/api/leo/api.rb
@@ -3,20 +3,21 @@ require 'grape-swagger'
 
 module Leo
   module JSendSuccessFormatter
-      def self.call object, env
-        { :status => 'ok', :data => object }.to_json
+    def self.call object, env
+      { :status => 'ok', :data => object }.to_json
+    end
+  end
+  module JSendErrorFormatter
+    def self.call message, backtrace, options, env
+      # This uses convention that a error! with a Hash param is a jsend "fail", otherwise we present an "error"
+      if message.is_a?(Hash)
+        { :status => 'fail', :data => message, backtrace: backtrace }.to_json
+      else
+        { :status => 'error', :message => message, backtrace: backtrace }.to_json
       end
     end
-    module JSendErrorFormatter
-      def self.call message, backtrace, options, env
-        # This uses convention that a error! with a Hash param is a jsend "fail", otherwise we present an "error"
-        if message.is_a?(Hash)
-          { :status => 'fail', :data => message, backtrace: backtrace }.to_json
-        else
-          { :status => 'error', :message => message, backtrace: backtrace }.to_json
-        end
-      end
-    end
+  end
+
   class API < Grape::API
     version 'v1', using: :path, vendor: 'leo-health'
     format :json
@@ -66,6 +67,7 @@ module Leo
     end
 
     mount Appointments
+    mount Conversations
     mount Sessions
     mount Statuses
     mount Users
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -11,4 +11,25 @@ class Conversation < ActiveRecord::Base
 	has_many :messages
 	has_and_belongs_to_many :participants, class_name: 'User', join_table: 'conversations_participants', association_foreign_key: 'participant_id'
 
+
+
+	def self.for_user(user)
+		if user.has_role? :parent
+			user.conversations 
+			# TODO need to figure out how conversations will be shared across multiple parents
+		elsif user.has_role? :guardian
+			user.conversations
+		elsif user.has_role? :child
+			#TODO: Implement
+		elsif user.has_role? :physician
+			#TODO: Implement
+		elsif user.has_role? :clinical_staff
+			#TODO: Implement
+		elsif user.has_role? :other_staff
+			#TODO: Implement
+		elsif user.has_role? :admin
+			Conversation.all
+		end
+	end
+
 end
