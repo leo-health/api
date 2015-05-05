dcffc6395209a41853b740bffda9864e84d2099a
Danish Munir <dtmunir@gmail.com>
2015-05-05 16:34:01 -0400

    Convert tabs to spaces

--- app/models/family.rb
+++ app/models/family.rb
@@ -8,38 +8,38 @@
 #
 
 class Family < ActiveRecord::Base
-	after_save :ensure_default_conversation_exists
+  after_save :ensure_default_conversation_exists
 
-	has_many :members, :class_name => 'User'
-	has_many :conversations
+  has_many :members, :class_name => 'User'
+  has_many :conversations
 
-	def conversation
-		ensure_default_conversation_exists
-		self.conversations.first
-	end
+  def conversation
+    ensure_default_conversation_exists
+    self.conversations.first
+  end
 
-	def parents
-		self.members.with_role :parent
-	end
+  def parents
+    self.members.with_role :parent
+  end
 
-	def guardians
-		self.members.with_role :guardian
-	end
+  def guardians
+    self.members.with_role :guardian
+  end
 
-	def children
-		self.members.with_role :child
-	end
+  def children
+    self.members.with_role :child
+  end
 
-	private
+  private
 
-	def ensure_default_conversation_exists
-		setup_default_conversation if self.conversations.count == 0 
-	end
+  def ensure_default_conversation_exists
+    setup_default_conversation if self.conversations.count == 0 
+  end
 
-	def setup_default_conversation
-		conversation = Conversation.new
-		conversation.family = self
-		conversation.participants << self.parents
-		conversation.save
-	end
+  def setup_default_conversation
+    conversation = Conversation.new
+    conversation.family = self
+    conversation.participants << self.parents
+    conversation.save
+  end
 end
