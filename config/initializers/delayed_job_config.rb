51bacaeed89d367423a2c443297b359fad2f7d35
davormrkoci <davormrkoci@gmail.com>
2015-05-06 14:59:57 -0400

    schedule sync tasks only if delayed_job has been initialized.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,8 +1,5 @@
-#don't delete entries for failed jobs
-Delayed::Worker.destroy_failed_jobs = true
-
 #don't delay jobs when running tests
 Delayed::Worker.delay_jobs = !%w[ test ].include?(Rails.env)
 
 #make sure that the sync job is scheduled.  It will reschedule automatically.
-ProcessSyncTasksJob.schedule #TODO: This line breaks the initializer if the delayed_jobs table isn't created
+ProcessSyncTasksJob.schedule if Delayed::Job.table_exists? 
