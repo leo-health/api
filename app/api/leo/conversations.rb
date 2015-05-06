e4afba5814e2438d797b0dee9c9058e73708418e
Danish Munir <dtmunir@gmail.com>
2015-05-06 15:46:13 -0400

    Add documentation for entities and fix a typo

--- app/api/leo/conversations.rb
+++ app/api/leo/conversations.rb
@@ -19,13 +19,13 @@ module Leo
       expose :conversation_id,  documentation: {type: "integer", desc: "The conversation id." }
       expose :body, documentation: {type: "string", desc: "The message body." }
       expose :message_type, documentation: {type: "string", desc: "The message type." }
-      expose :created_at
+      expose :created_at, , documentation: {type: "datetime", desc: "The date/time the message was created at." }
       expose :escalated_to, with: Leo::Entities::ConversationParticipantEntity, documentation: {type: "object", desc: "The physician who the message has been escalated to." }
-      expose :escalated_by, with: Leo::Entities::ConversationParticipantEntity
-      expose :escalated_at
-      expose :resolved_requested_at
-      expose :resolved_approved_at
-      expose :resolved
+      expose :escalated_by, with: Leo::Entities::ConversationParticipantEntity, documentation: {type: "object", desc: "The staff who the message has been escalated by." }
+      expose :escalated_at, documentation: {type: "datetime", desc: "The date/time the message was escalated at." }
+      expose :resolved_requested_at, documentation: {type: "datetime", desc: "The date/time a physician marked a message as resolved." }
+      expose :resolved_approved_at, documentation: {type: "datetime", desc: "The date/time a staff member approved a resolved request." }
+      expose :resolved, documentation: {type: "boolean", desc: "Has the escalation been resolved?" }
       expose :read_receipts
     end
 
@@ -215,7 +215,7 @@ module Leo
             params do 
             end
             post "approve_resolved" do 
-              if current_user.id != @message.escalate_ny_id
+              if current_user.id != @message.escalate_by_id
                 error({error_code: 403, error_message: "You are not allowed to approve a request to mark this message as resolved."})
                 return
               end
