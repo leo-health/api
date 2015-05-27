a6dc61a07823a2a95a4e50fda082df3dbf1d1a77
Danish Munir <dtmunir@gmail.com>
2015-05-27 15:11:48 -0400

    Set up before filter and after_validation filter to load the current user for all calls to users/:id namespaced endpoints

--- app/api/leo/users.rb
+++ app/api/leo/users.rb
@@ -86,10 +86,17 @@ module Leo
 
       desc "Calls specific to a user"
       route_param :id do 
+        before do
+          puts "users/:id - before"
+          authenticated_user
+        end
+        after_validation do
+          puts "users/:id - after_validation"
+          @user = User.find(params[:id])
+        end
         desc "Return a user"
         get do
-          authenticated_user
-          user = User.find(params[:id])
+          present :user, @user, with: Leo::Entities::UserEntity
         end
         
         namespace :invitations do
@@ -97,11 +104,11 @@ module Leo
           # GET users/:id/invitations
           desc "Get invitations sent by this user"
           get do
-            if user != current_user
+            if @user != current_user
               error!({error_code: 403, error_message: "You don't have permission to list this user's invitiations."}, 403)
               return
             end
-            invitations = user.invitations
+            invitations = @user.invitations
             present :invitations, invitations, with: Leo::Entities::UserEntity
           end
 
@@ -113,9 +120,10 @@ module Leo
             requires :email,      type: String, desc: "Email", user_unique: true
             requires :family_id,  type: Integer, desc: "Family Id for the new user"
             requires :dob,        type: String, desc: "Date of Birth"
+            requires :sex,        type: String, desc: "Sex", values: ['M', 'F', 'U']
           end
           post do
-            if user != current_user
+            if @user != current_user
               error!({error_code: 403, error_message: "You don't have permission to list this user's invitiations."}, 403)
               return
             end
@@ -129,7 +137,7 @@ module Leo
             # Check that family exists and this user has access to that family
             family_id = params[:family_id]
             family = Family.find_by_id(family_id)
-            if family.nil? or family_id != user.family_id
+            if family.nil? or family_id != @user.family_id
               error!({error_code: 422, error_message: "Invalid family"},422)
               return
             end
@@ -139,20 +147,13 @@ module Leo
               first_name:   params[:first_name],
               last_name:    params[:last_name],
               dob:          dob,
-              family_id:    family.id
+              family_id:    family.id,
+              sex:          params[:sex]
               ) do |u|
-              u.invited_by_id =  user.id
+              u.invited_by_id =  @user.id
               u.invited_by_type = 'User'
             end
-            # user = User.create!(
-            # {
-            #   first_name:   params[:first_name],
-            #   last_name:    params[:last_name],
-            #   email:        params[:email],
-            #   password:     params[:password],
-            #   dob:          dob,
-            #   family_id:    family.id
-            # })
+            
             invited_user.add_role :parent
             family.conversation.participants << invited_user
             present :user, invited_user, with: Leo::Entities::UserEntity
@@ -163,7 +164,7 @@ module Leo
             requires :user_id,         type: Integer, desc: "Id for user who's invitation is to be deleted"
           end
           delete do
-            if user != current_user
+            if @user != current_user
               error!({error_code: 403, error_message: "You don't have permission to delete this user's invitiations."}, 403)
               return
             end
@@ -174,7 +175,7 @@ module Leo
               return
             end
 
-            if !user_to_delete.invitation_accepted_at.nil? or user_to_delete.family.primary_parent.id != user.id or user_to_delete == user
+            if !user_to_delete.invitation_accepted_at.nil? or user_to_delete.family.primary_parent.id != @user.id or user_to_delete == @user
               error!({error_code: 403, error_message: "You don't have permission to delete this invitation or it is no longer pending."}, 403)
               return
             end
@@ -186,11 +187,11 @@ module Leo
 
           # GET users/:id/children
           get do
-            if user != current_user
+            if @user != current_user
               error!({error_code: 403, error_message: "You don't have permission to list this user's children."}, 403)
               return
             end
-            children = user.family.children
+            children = @user.family.children
             present :children, children, with: Leo::Entities::UserEntity
           end
 
@@ -204,7 +205,7 @@ module Leo
             requires :sex,        type: String, desc: "Sex", values: ['M', 'F', 'U']
           end
           post do
-            if user != current_user
+            if @user != current_user
               error!({error_code: 403, error_message: "You don't have permission to add a child for this user."}, 403)
               return
             end
@@ -215,10 +216,10 @@ module Leo
               return
             end
 
-            family = user.family
+            family = @user.family
             role = Role.where(name: :child)
             
-            user = User.create!(
+            child = User.create!(
             {
               first_name:   params[:first_name],
               last_name:    params[:last_name],
@@ -229,8 +230,8 @@ module Leo
             })
 
             
-            user.add_role :child
-            present :user, user, with: Leo::Entities::UserEntity
+            child.add_role :child
+            present :child, child, with: Leo::Entities::UserEntity
           end
 
         end
