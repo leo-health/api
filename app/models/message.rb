9c847a4380ed1b1652c84ba3ebb7f3a18113353b
Danish Munir <dtmunir@gmail.com>
2015-06-03 10:15:12 -0400

    Update the Conversation last_message_created timestamp to Fix leo-health/parent-webapp#15

--- app/models/message.rb
+++ app/models/message.rb
@@ -22,6 +22,8 @@ class Message < ActiveRecord::Base
   belongs_to :escalated_to, class_name: 'User'
   belongs_to :escalated_by, class_name: 'User'
 
+  after_save :update_conversation_last_message_timestamp, on: [:create]
+
   def read_by!(user)
     r = self.read_receipts.new(participant: user)
     r.save
@@ -29,6 +31,13 @@ class Message < ActiveRecord::Base
 
   def resolved
     !self.resolved_approved_at.nil?
+  end
+
+  private
+
+  def update_conversation_last_message_timestamp
+    conversation = self.conversation
+    conversation.update_attribute(:last_message_created, self.created_at)
   end
 
 end
