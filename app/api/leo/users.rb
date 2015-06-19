e456d0c6ddfb8eff3560ef4cc6819975540ffb6f
Wuang Qin <wuang@leohealth.com>
2015-06-19 16:29:31 -0400

    continue refactoring the users api

--- app/api/leo/users.rb
+++ app/api/leo/users.rb
@@ -103,7 +103,7 @@ module Leo
         present :user, user, with: Leo::Entities::UserWithAuthEntity
       end
 
-      desc "Calls specific to a user"
+      desc "#show single user"
       route_param :id do 
         before do
           authenticated_user
@@ -113,19 +113,17 @@ module Leo
         end
         desc "Return a user"
         get do
-          puts "In get user"
           present :user, @user, with: Leo::Entities::UserEntity
         end
 
         params do
           requires :stripe_token,  type: String, desc: "Stripe Token to use for creating Stripe token"
         end
+        #extract the stripe part out later from users api
         post "/add_card" do
-          puts "In add_card"
           token = params[:stripe_token]
-          if token.blank? or token.nil?
-            error!({error_code: 422, error_message: "A valid stripe token is required."}, 422)
-            return
+          unless token || token.blank?
+            error!({error_code: 422, error_message: "A valid stripe token is required."}, 422) and return
           end
           # Save the customer ID in user table so you can use it later
           @user.create_or_update_stripe_customer_id(token)
@@ -141,7 +139,6 @@ module Leo
           optional :sex,        type: String, desc: "Sex", values: ['M', 'F', 'U']
         end
         put do
-          puts "In Update User"
           sanitized_params = declared(params, {include_missing: false})
           if sanitized_params.key?('role')
             role = Role.where(name: sanitized_params[:role])
@@ -245,8 +242,7 @@ module Leo
           end
         end
 
-        namespace :children do 
-
+        namespace :children do
           # GET users/:id/children
           get do
             puts "In get /users/#{params[:id]}/children"
@@ -281,27 +277,22 @@ module Leo
             end
 
             family = @user.family
-            role = Role.where(name: :child)
-            
-            child = User.new(
-            {
-              first_name:   params[:first_name],
-              last_name:    params[:last_name],
-              email:        params[:email],
-              dob:          dob,
-              family_id:    family.id,
-              sex:          params[:sex]
-            })
 
-            child.add_role :child
-            child.save!
+            child_params = { first_name: params[:first_name],
+                              last_name: params[:last_name],
+                              email: params[:email],
+                              dob: dob,
+                              family_id: family.id,
+                              sex: params[:sex] }
+            
+            if child = User.create(child_params)
+              child.add_role :child
+              child.save!
+            end
             present :user, child, with: Leo::Entities::UserEntity
           end
-
         end
       end
-
-
     end
   end
 end
