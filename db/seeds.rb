eed432ccbe10cad6de86fa0bf315e0813c7bbd94
Wuang Qin <wuang@leohealth.com>
2015-06-24 17:32:15 -0400

    fix indentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 ROLES = {
-					# Admin is 1
-					admin: 1,
-					# Leo users are 10-19
-					physician: 11,
-					clinical_staff: 12,
-					other_staff: 13,
-					# Leo customers are 20-29
-					parent: 21,
-					child: 22,
-					guardian: 23
+          # Admin is 1
+          admin: 1,
+          # Leo users are 10-19
+          physician: 11,
+          clinical_staff: 12,
+          other_staff: 13,
+          # Leo customers are 20-29
+          parent: 21,
+          child: 22,
+          guardian: 23
         }
 
 ROLES.each do |role, id|
-	Role.update_or_create_by_id_and_name(id, role) do |r|
-		r.save
-	end
+  Role.update_or_create_by_id_and_name(id, role) do |r|
+    r.save
+  end
 end
