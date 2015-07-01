e2911b2da8d8fd17192dc23195aa822b8ad35b30
Nayan Jain <supernayan@Nayans-MacBook-Pro.local>
2015-07-01 14:54:33 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
+# Groups:
+#   Super User:
+#     0
+#   Staff:
+#     1, 2, 3, 5
+#   Family:
+#     4, 6
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 ROLES = {
           # Super user / admin
           super_user: 0,
           # Accounting and billing
           billing: 1,
-          # Non-provider roles (MA, nurse, NP)
+          # Non-provider roles (medical assistant, nurse practicioner)
           clinical_support: 2,
           # Leo staff and reception
           customer_service: 3,
           # Parent and legal guardians
           parent: 4,
-          # Pediatricians and other providers
+          # Pediatricians and other care providers
           physician: 5,
-          #Children
+          # Children
           child: 6
         }
 
