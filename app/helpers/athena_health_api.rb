1f2f8474ccd85661674fac236ee6d7ccf4f69081
davormrkoci <davormrkoci@gmail.com>
2015-07-09 15:07:27 -0400

    Fix for Athena authentication token changes.

--- app/helpers/athena_health_api.rb
+++ app/helpers/athena_health_api.rb
@@ -118,10 +118,13 @@ module AthenaHealthAPI
 
       response = @connection.request(request)
 
+      Rails.logger.info("response code: #{response.code}") if Connection.debug
+      Rails.logger.info("response body: #{response.body}") if Connection.debug
+
       raise "Athena authentication failed: code #{response.code}" unless response.code == "200"
 
       authorization = JSON.parse(response.body)
-      @@last_token = @token = authorization['authentication_token']
+      @@last_token = @token = authorization['access_token']
     end
     
     # Joins together URI paths so we can use it in API calls.  Trims extra slashes from arguments,
@@ -252,8 +255,6 @@ module AthenaHealthAPI
       request = Net::HTTP::Delete.new(path_join(@version, @practiceid, url))
       return call(request, {}, headers)
     end
-    
-  private :authenticate, :path_join, :call
     
   end
   
