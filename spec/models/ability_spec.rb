6c4fdf0830756a4e1052216ac5ea937c3199d731
Wuang Qin <wuang@leohealth.com>
2015-07-16 17:54:28 -0400

    added the test for ability model

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -5,7 +5,7 @@ describe "User" do
   describe "abilities" do
     subject(:ability){ Ability.new(user) }
 
-    context "when is an account manager" do
+    context "when is an super user" do
       let!(:user){create(:user)}
 
       before do
@@ -14,5 +14,13 @@ describe "User" do
 
       it{ should be_able_to(:manage, User.new) }
     end
+
+    context "when is a guardian" do
+     let(:patient){create(:user, :child)}
+     let!(:family){patient.family}
+     let!(:user){create(:user, :father, family: family)}
+
+     it{should be_able_to(:crud, patient)}
+     end
   end
 end
