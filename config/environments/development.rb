dadada1b474efee71f2540c0ff562ce3d60a9993
Wuang Qin <wuang@leohealth.com>
2015-07-20 16:07:14 -0400

    added the config file for the mandrill mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :letter_opener
 
+  config.mandrill_mailer.default_url_options = { :host => 'localhost' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
