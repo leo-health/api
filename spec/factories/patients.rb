108dd3e25b41fb98228ea0b4e21eddd36294d81b
Wuang Qin <wuang@leohealth.com>
2015-07-21 18:19:36 -0400

    fix the indentation for patient factory

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :patient do
-    first_name 	{ ['Danish', 'Wuang', 'Zach', 'Ben', 'Nayan'].sample }
-    last_name 	{ ['Munir', 'Kale', 'Freeman', 'Singh'].sample }
-    birth_date 	{ 29.years.ago.to_s }
-    sex					{ ['M', 'F'].sample }
+    first_name { ['Danish', 'Wuang', 'Zach', 'Ben', 'Nayan'].sample }
+    last_name { ['Munir', 'Kale', 'Freeman', 'Singh'].sample }
+    birth_date { 29.years.ago.to_s }
+    sex { ['M', 'F'].sample }
     association :family, factory: :family
   end
 end
