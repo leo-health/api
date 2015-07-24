b4595c905e255e261a18add26873556d5975ad0d
Wuang Qin <wuang@leohealth.com>
2015-07-24 10:46:23 -0400

    fix the wrong schema, by rolling back the migration, and re-run migration

--- db/schema.rb
+++ db/schema.rb
@@ -272,13 +272,10 @@ ActiveRecord::Schema.define(version: 20150724140705) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
-    t.integer  "resource_id"
-    t.string   "resource_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
   create_table "sessions", force: :cascade do |t|
