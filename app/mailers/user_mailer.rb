4d4e02c62c9b2bd3da54c7e505ddc72722d516e2
Wuang Qin <wuang@leohealth.com>
2015-07-29 16:58:39 -0400

    add the confirmation instrunction for confirm user's email into user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,7 +12,20 @@ class UserMailer < MandrillMailer::TemplateMailer
     )
   end
 
-  def reset_password(user, token)
+  def confirmation_instructions(user, token, opts={})
+    @token = token
+    mandrill_mail(
+      template: 'confirm_email',
+      subject: 'Please confirm your account',
+      to: user.email,
+      vars: {
+        'EMAIL' => user.email,
+        'LINK' => "http://localhost:8888/#/changePassword?reset_password_token=#{token}"
+      }
+    )
+  end
+
+  def reset_password_instructions(user, token, opts={})
     mandrill_mail(
       template: 'reset_password',
       subject: 'reset_password',
