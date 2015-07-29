f7b1d5113260377ef1ef326fa8f74641c80864ba
Wuang Qin <wuang@leohealth.com>
2015-07-29 16:20:47 -0400

    autoload for custom jobs

--- app/jobs/mail/reset_password_job.rb
+++ app/jobs/mail/reset_password_job.rb
@@ -1,6 +1,6 @@
 class ResetPasswordJob < Struct.new(:user_id, :token)
   def perform
-    user = User.find(user_id)
+    user = User.try(:find, user_id)
     UserMailer.reset_password(user, token).deliver if user
   end
 end
