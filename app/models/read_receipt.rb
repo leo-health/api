5478774a7af3d05068b8672ee1569abbc56bb045
Wuang Qin <wuang@leohealth.com>
2015-08-13 16:09:47 -0400

    add the read_receipt endpoint

--- app/models/read_receipt.rb
+++ app/models/read_receipt.rb
@@ -1,4 +1,7 @@
 class ReadReceipt < ActiveRecord::Base
   belongs_to :message
   belongs_to :reader, class_name: 'User'
+
+  validates :messages, :reader, presence: true
+  validates_uniqueness_of :reader_id, scope: :message_id
 end
