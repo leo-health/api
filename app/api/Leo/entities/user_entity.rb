5f38536fb035bffaad9df5334faf87395a63a70a
Wuang Qin <wuang@leohealth.com>
2015-08-17 11:27:05 -0400

    change the user entity to match the case of provider and staff

--- app/api/Leo/entities/user_entity.rb
+++ app/api/Leo/entities/user_entity.rb
@@ -1,7 +1,21 @@
 module Leo
   module Entities
     class UserEntity < Grape::Entity
-      expose :id, :title, :first_name, :middle_initial, :last_name, :dob, :sex, :practice_id, :family_id, :email, :stripe_customer_id, :role
+      expose :id, :title, :first_name, :middle_initial, :last_name, :dob, :sex, :practice_id, :family_id, :email, :stripe_customer_id
+      expose :credentials, if: {role_id: 5}
+      expose :specialties, if: {role_id: 5}
+      expose :role
+      expose :role_id
+
+      private
+
+      def role
+        object.role.name
+      end
+
+      def role_id
+        object.role.id
+      end
     end
   end
 end
