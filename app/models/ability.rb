b2316b74778c7a930c75c8e282f9ef7fb4d38196
Wuang Qin <wuang@leohealth.com>
2015-08-20 11:48:37 -0400

    add the ability of User with tests

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,26 +6,32 @@ class Ability
     if user.has_role? :super_user
       can :manage, :all
     elsif user.has_role? :guardian
+      can :read, User, :role_id => [1, 2, 3, 4, 5]
+      can [:create, :update, :destroy], User, :family_id => user.family_id
       can [:read, :update, :destroy], Patient, :family_id => user.family_id
       can :read, Conversation, :family_id => user.family_id
       can :read, Message, :sender_id => user.id
       can :create, Message, :conversation_id => Conversation.find_by_family_id(user.family_id).id
     elsif user.has_role? :financial
+      can :read, User, :role_id => [1, 2, 3, 4, 5]
       can [:read, :update], Conversation
       can [:create, :read], Message
       can :update, Message, :escalated_at => nil
       can :update, UserConversation
     elsif user.has_role? :clinical
+      can [:read, :update, :destroy, :create], User, :role_id => [1, 2, 3, 4, 5]
       can [:read, :update], Conversation
       can [:create, :read], Message
       can :update, Message, :escalated_at => nil
       can :update, UserConversation
     elsif user.has_role? :clinical_support
+      can [:read, :update, :destroy, :create], User, :role_id => [1, 2, 3, 4, 5]
       can [:read, :update], Conversation
       can [:create, :read], Message
       can :update, Message, :escalated_at => nil
       can :update, UserConversation
     elsif user.has_role? :customer_service
+      can :read, User, :role_id => [1, 2, 3, 4, 5]
       can [:read, :update], Conversation
       can [:create, :read], Message
       can :update, Message, :escalated_at => nil
