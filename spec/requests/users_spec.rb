81a56b1d18ded66ffb3cae2b1750117fcdee082d
Wuang Qin <wuang@leohealth.com>
2015-08-20 12:12:30 -0400

    adjust user requests spec according to user ability change

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -56,28 +56,33 @@ describe Leo::V1::Users do
   end
 
   describe "DELETE /api/v1/users/id" do
-    let!(:super_user){create(:role, :super_user)}
-    let!(:clinical){create(:role, :clinical)}
-    let(:user){create(:user, role: super_user)}
-    let!(:session){user.sessions.create}
     let!(:deleted_user){create(:user)}
 
     def do_request
       delete "/api/v1/users/#{deleted_user.id}", {authentication_token: session.authentication_token}
     end
 
-    it "should delete selected user if current user has admin right" do
-      expect(User.count).to eq(2)
-      do_request
-      expect(response.status).to eq(200)
-      expect(User.count).to eq(1)
+    context "has the right to delete user" do
+      let(:user){create(:user, :clinical)}
+      let!(:session){user.sessions.create}
+
+      it "should delete selected user if current user has the right" do
+        expect(User.count).to eq(2)
+        do_request
+        expect(response.status).to eq(200)
+        expect(User.count).to eq(1)
+      end
     end
 
-    it "should not delete selected user and raise error when user do not have the access right" do
-      user.add_role :clinical
-      do_request
-      expect(response.status).to eq(403)
-      expect(User.count).to eq(2)
+    context "do not has the right to delete user" do
+      let(:user){create(:user, :financial)}
+      let!(:session){user.sessions.create}
+
+      it "should not delete selected user and raise error when user do not have the access right" do
+        do_request
+        expect(response.status).to eq(403)
+        expect(User.count).to eq(2)
+      end
     end
   end
 end
