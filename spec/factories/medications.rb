6a3a337958957ea397544f0a39161b0512c2d941
Wuang Qin <wuang@leohealth.com>
2015-08-25 10:05:04 -0400

    set up active record association and the factory

--- spec/factories/medications.rb
+++ spec/factories/medications.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :medication do
-    
+    association :health_record, strategy: :build
   end
-
 end
--- spec/factories/vaccines.rb
+++ spec/factories/vaccines.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :vaccine do
-    
+    association :health_record, strategy: :build
   end
-
 end
