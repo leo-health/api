b8d4fa29337887874a7e8c535792dd54cab422c1
Wuang Qin <wuang@leohealth.com>
2015-08-26 15:29:05 -0400

    change the cards response structure

--- app/api/Leo/v1/cards.rb
+++ app/api/Leo/v1/cards.rb
@@ -11,8 +11,7 @@ module Leo
           family = Family.includes(:guardians).find(current_user.family_id)
           appointments = Appointment.where( :booked_by_id => family.guardians.pluck(:id)).order("created_at DESC")
           cards = appointments.each_with_index.inject([]){|cards, (appointment, index)| cards << {card_data: appointment, priority: index, type: "appointment", type_id: 0}}
-          present :count, appointments.count
-          present :cards, cards, with: Leo::Entities::CardEntity
+          present cards, with: Leo::Entities::CardEntity
         end
       end
     end
