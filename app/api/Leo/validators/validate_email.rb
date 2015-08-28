0f9cbbded63acd45010dd0bbceeb061144a84112
Wuang Qin <wuang@leohealth.com>
2015-08-28 12:12:04 -0400

    document the source of regex used in email validator

--- app/api/Leo/validators/validate_email.rb
+++ app/api/Leo/validators/validate_email.rb
@@ -1,6 +1,7 @@
 module Leo
   module Validators
     class ValidateEmail < Grape::Validations::Base
+      #source of the regex https://github.com/balexand/email_validator
       def validate_param!(attr_name, params)
         unless !!( params[attr_name] =~ /\A\s*([^@\\s]{1,64})@((?:[-\p{L}\d]+\.)+\p{L}{2,})\s*\z/i )
           fail Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: 'Email format is not correct'
