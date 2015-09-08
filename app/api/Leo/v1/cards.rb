caff7d00fef57c6b67190c7d995eee26543993c2
Wuang Qin <wuang@leohealth.com>
2015-09-08 14:16:24 -0400

    card_entity, card_endpoint with conversation card, sort cards in order, add tests

--- app/api/Leo/v1/cards.rb
+++ app/api/Leo/v1/cards.rb
@@ -10,7 +10,14 @@ module Leo
         get do
           family = Family.includes(:guardians).find(current_user.family_id)
           appointments = Appointment.where( :booked_by_id => family.guardians.pluck(:id)).order("created_at DESC")
-          cards = appointments.each_with_index.inject([]){|cards, (appointment, index)| cards << {card_data: appointment, priority: index, type: "appointment", type_id: 0}}
+          upcoming_appointments, past_appointment = appointments.partition{|appointment| appointment.start_datetime > Time.now}
+          cards = (upcoming_appointments + [family.conversation] + past_appointment).each_with_index.inject([]) do |cards, (card, index)|
+            if card.class == Conversation
+              cards << {conversation_card_data: card, priority: index, type: 'conversation', type_id: 1}
+            else
+              cards << {appointment_card_data: card, priority: index, type: 'appointment', type_id: 0}
+            end
+          end
           present cards, with: Leo::Entities::CardEntity
         end
       end
