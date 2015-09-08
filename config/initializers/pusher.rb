d352565f9b2995e682cddca04f15a34a4eb44d7a
Wuang Qin <wuang@leohealth.com>
2015-09-08 15:02:19 -0400

    add pusher fake for pusher related testing

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,5 +1,14 @@
 #TODO save keys on server
-Pusher.app_id = '131452'
-Pusher.key = '218006d766a6d76e8672'
-Pusher.secret = '988ccf7344c190c723fc'
-Pusher.logger = Rails.logger
+if Rails.env.test?
+  # Ensure Pusher configuration is set if you're not doing so elsewhere.
+  Pusher.app_id = "MY_TEST_ID"
+  Pusher.key    = "MY_TEST_KEY"
+  Pusher.secret = "MY_TEST_SECRET"
+
+  # Require the base file, which immediately starts the socket and web servers.
+else
+  Pusher.app_id = '131452'
+  Pusher.key = '218006d766a6d76e8672'
+  Pusher.secret = '988ccf7344c190c723fc'
+  Pusher.logger = Rails.logger
+end
