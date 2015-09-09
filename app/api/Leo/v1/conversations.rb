12811ae74c722196b85c03db59d8bea26e35e77f
Wuang Qin <wuang@leohealth.com>
2015-09-09 11:03:43 -0400

    change read to update auth check on the conversation update endpoint

--- app/api/Leo/v1/conversations.rb
+++ app/api/Leo/v1/conversations.rb
@@ -11,7 +11,7 @@ module Leo
         desc "Close a conversation"
         put ":id" do
           conversation = Conversation.find(params[:id])
-          authorize! :read, conversation
+          authorize! :update, conversation
           if conversation.update_attributes(status: "closed", last_closed_at: Time.now, last_closed_by: current_user.id)
             present :conversation, conversation, with: Leo::Entities::ConversationEntity
           end
