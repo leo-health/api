d0fe72fb5e00352f7204e9065c1ba007e0c9e731
Wuang Qin <wuang@leohealth.com>
2015-09-09 14:21:47 -0400

    display message_type as type in the response

--- app/api/Leo/entities/message_entity.rb
+++ app/api/Leo/entities/message_entity.rb
@@ -5,7 +5,7 @@ module Leo
       expose :sender, with: Leo::Entities::UserEntity
       expose :conversation_id
       expose :body
-      expose :message_type
+      expose :message_type, as: :type
       expose :created_at
       expose :escalated_to, with: Leo::Entities::UserEntity
       expose :escalated_by, with: Leo::Entities::UserEntity
