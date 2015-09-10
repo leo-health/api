e8e0a645a2d4f8adf21ac06e1b835a878a42ee18
Wuang Qin <wuang@leohealth.com>
2015-09-10 16:35:02 -0400

    change type to type_name on card_entity

--- app/api/Leo/entities/card_entity.rb
+++ app/api/Leo/entities/card_entity.rb
@@ -2,7 +2,7 @@ module Leo
   module Entities
     class CardEntity < Grape::Entity
       expose :priority
-      expose :type
+      expose :type, as: :type_name
       expose :type_id
       expose :conversation_card_data, using: Leo::Entities::ConversationEntity, as: :card_data, if: Proc.new {|g| g[:conversation_card_data]}
       expose :appointment_card_data, using: Leo::Entities::AppointmentEntity, as: :card_data, if: Proc.new {|g| g[:appointment_card_data]}
