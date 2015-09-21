4d854e9f9bc3395976ca87d119ab6364d909d3b3
Wuang Qin <wuang@leohealth.com>
2015-09-21 17:17:55 -0400

    fix the schema issue, dup deletion on appointment_status_id

--- db/migrate/20150908193918_change_status_column_on_appointments_table.rb
+++ db/migrate/20150908193918_change_status_column_on_appointments_table.rb
@@ -1,7 +1,6 @@
 class ChangeStatusColumnOnAppointmentsTable < ActiveRecord::Migration
   def up
     remove_column :appointments, :status
-    remove_column :appointments, :status_id
     add_column :appointments, :appointment_status_id, :integer, null: false
     add_index :appointments, :appointment_status_id
   end
@@ -9,6 +8,5 @@ class ChangeStatusColumnOnAppointmentsTable < ActiveRecord::Migration
   def down
     add_column :appointments, :status, :string, null: false
     remove_column :appointments, :appointment_status_id
-    add_column :appointments, :status_id, :integer, null: false
   end
 end
