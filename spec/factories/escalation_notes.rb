3d88255d80bdc72f075757bb697d7e77ec568603
Wuang Qin <wuang@leohealth.com>
2015-09-24 12:06:26 -0400

    add the escalation_notes factory

--- spec/factories/escalation_notes.rb
+++ spec/factories/escalation_notes.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :escalation_note do
-    
+    association :message, strategy: :build
+    association :assignor, factory: [:user, :customer_service], strategy: :build
+    association :assignee, factory: [:user, :clinical], strategy: :build
+    priority_level "standard"
+    note "message escalted to you"
   end
-
 end
