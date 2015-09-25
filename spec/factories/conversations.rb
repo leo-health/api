099da2292d170d01bea6fd9a0aaeb8c16ba3d9d5
Wuang Qin <wuang@leohealth.com>
2015-09-25 17:52:40 -0400

    add trait to conversation factory, and finishi testing conversation model

--- spec/factories/conversations.rb
+++ spec/factories/conversations.rb
@@ -2,5 +2,17 @@ FactoryGirl.define do
   factory :conversation do
     association :family, strategy: :build
     status :closed
+
+    trait :open do
+      after(:build) do |conversation|
+        conversation.update_attributes(status: :open)
+      end
+    end
+
+    trait :escalated do
+      after(:build) do |conversation|
+        conversation.update_attributes(status: :escalated)
+      end
+    end
   end
 end
