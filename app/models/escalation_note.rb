3ac0a1fcad697e06889f597938a0109605aad5d4
Wuang Qin <wuang@leohealth.com>
2015-09-25 10:56:59 -0400

    change relations on user_conversation model, and add specs for the model

--- app/models/escalation_note.rb
+++ app/models/escalation_note.rb
@@ -1,6 +1,5 @@
 class EscalationNote < ActiveRecord::Base
-  belongs_to :conversation
-  belongs_to :escalated_to,  ->{where('role_id != ?', 4)}, class_name: 'User'
+  belongs_to :user_conversation
   belongs_to :escalated_by,  ->{where('role_id != ?', 4)}, class_name: 'User'
 
   validates :escalated_to, :escalated_by, :conversation, :priority, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:last_name) }
     it { is_expected.to validate_presence_of(:role) }
     it { is_expected.to validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
   end
 end
