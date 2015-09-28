14cea639614385b6552fc8dcb29c524128f80d3b
Wuang Qin <wuang@leohealth.com>
2015-09-28 18:21:48 -0400

    update enrollment with a authentication token

--- app/api/Leo/v1/enrollments.rb
+++ app/api/Leo/v1/enrollments.rb
@@ -18,14 +18,16 @@ module Leo
 
         desc "update an enrollment"
         params do
+          requires :authentication_token, type: String, allow_blank: false
           optional :first_name, type: String
           optional :last_name, type: String
           optional :birth_date, type: Date
           optional :sex, type: String, values: ['M', 'F']
           optional :stripe_customer_id, type: String
         end
-        put ":id" do
-          enrollment = Enrollment.find(params[:id])
+        put :current do
+          enrollment = Enrollment.find_by_authentication_token(params[:authentication_token])
+          error!({ error_code: 401, error_message: '401 Unauthorized' }, 401) unless enrollment
           if enrollment.update_attributes(declared(params, include_missing: false))
             present :enrollment, enrollment
           else
