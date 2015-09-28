86a8608dcbcc5c5d9f9aad0397fb7a057fbdb7bc
Wuang Qin <wuang@leohealth.com>
2015-09-28 17:56:39 -0400

    ensure auth_tokens on enrollment table

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,9 +1,11 @@
 class Enrollment < ActiveRecord::Base
   devise :database_authenticatable
+  acts_as_token_authenticatable
   acts_as_paranoid
 
   has_many :patient_enrollments, foreign_key: "guardian_enrollment_id"
 
   validates :password, length: {minimum: 8, allow_nil: true}
-  validates :email, presence: true
+  validates :email, :authentication_token, presence: true
+  validates :authentication_token, uniqueness: {scope: :deleted_at}
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -4,7 +4,6 @@ class Session < ActiveRecord::Base
 
   belongs_to :user
 
-  before_validation :ensure_authentication_token, on: [:create, :update]
   validates :user, :authentication_token, presence: true
   validates :authentication_token, uniqueness: {scope: :deleted_at}
 end
