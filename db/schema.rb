a22516829014c226874af33874de5479b161392d
Wuang Qin <wuang@leohealth.com>
2015-10-01 15:51:24 -0400

    supply phone number to tests and enrollment table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001191516) do
+ActiveRecord::Schema.define(version: 20151001195003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,6 +167,7 @@ ActiveRecord::Schema.define(version: 20151001191516) do
     t.string   "authentication_token"
     t.boolean  "invited_user",         default: false
     t.integer  "insurance_plan_id"
+    t.string   "phone_number"
   end
 
   add_index "enrollments", ["authentication_token"], name: "index_enrollments_on_authentication_token", using: :btree
