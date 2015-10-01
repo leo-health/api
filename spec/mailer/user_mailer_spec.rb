3003ba12c23d7090c7ce3dd32cebe9a731aff482
Nayan Jain <nayan@leohealth.com>
2015-10-01 11:31:44 -0400

    209 - Update API to reflect tempalte changes

--- spec/mailer/user_mailer_spec.rb
+++ spec/mailer/user_mailer_spec.rb
@@ -11,7 +11,7 @@ describe UserMailer do
 
     it "should sent the user a confirmation_instructions email" do
       UserMailer.confirmation_instructions(user, "token").deliver
-      email = MandrillMailer::deliveries.detect { |mail| mail.template_name == 'Leo email confirmation' && mail.message['to'].any? { |to| to[:email] = "test@leohealth.com" } }
+      email = MandrillMailer::deliveries.detect { |mail| mail.template_name == 'Leo - Sign Up - Confirmation' && mail.message['to'].any? { |to| to[:email] = "test@leohealth.com" } }
       expect(email).to_not be_nil
     end
   end
@@ -21,7 +21,7 @@ describe UserMailer do
 
     it "should sent the user a reset_password_instructions email" do
       UserMailer.reset_password_instructions(user, "token").deliver
-      email = MandrillMailer::deliveries.detect { |mail| mail.template_name == 'Leo - Reset Password' && mail.message['to'].any? { |to| to[:email] = "test@leohealth.com" } }
+      email = MandrillMailer::deliveries.detect { |mail| mail.template_name == 'Leo - Password - Reset Password' && mail.message['to'].any? { |to| to[:email] = "test@leohealth.com" } }
       expect(email).to_not be_nil
     end
   end
