2b9c45b098f241c28a1064ac355d7ae77638922f
Davor Mrkoci <davormrkoci@gmail.com>
2015-10-19 11:42:18 -0400

    fix factory spec errors.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,6 +4,7 @@ class Patient < ActiveRecord::Base
   belongs_to :role
   has_many :appointments
   has_many :medications
+  has_many :allergies
   has_many :photos
   has_many :vaccines
   has_many :vitals
