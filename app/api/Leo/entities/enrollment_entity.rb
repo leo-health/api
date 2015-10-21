f0aa2c5fc5f6d7470ab57fcbf9fe5385454be320
Nayan Jain <nayan@leohealth.com>
2015-10-21 11:31:05 -0400

    noissue - update enrollments endpoint to move authentication_token up one level

---
+++ app/api/Leo/entities/enrollment_entity.rb
@@ -0,0 +1,29 @@
+module Leo
+  module Entities
+    class EnrollmentEntity < Grape::Entity
+      expose :id
+      expose :family_id
+      expose :title
+      expose :first_name
+      expose :middle_initial
+      expose :last_name
+      expose :birth_date
+      expose :suffix
+      expose :sex
+      expose :practice_id
+      #expose :practice, with: Leo::Entities::PracticeEntity
+      expose :email
+      expose :phone
+      expose :created_at
+      expose :deleted_at
+      expose :updated_at
+      expose :insurance_plan_id
+      expose :invited_user
+      expose :role_id
+      expose :avatar_url
+    end
+  end
+end
+
+
+
