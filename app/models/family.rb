354d431dbfb3a4a1c95e3a056484845fbcae2a5e
Wuang Qin <wuang@leohealth.com>
2015-11-06 12:15:49 -0500

    refactor conversation specs

--- app/models/family.rb
+++ app/models/family.rb
@@ -16,6 +16,6 @@ class Family < ActiveRecord::Base
   private
 
   def set_up_conversation
-    Conversation.find_or_create_by(family_id: id, state: :closed)
+    Conversation.create(family_id: id, state: :closed) unless Conversation.find_by_family_id(id)
   end
 end
--- spec/factories/conversations.rb
+++ spec/factories/conversations.rb
@@ -2,17 +2,5 @@ FactoryGirl.define do
   factory :conversation do
     association :family, strategy: :build
     state :closed
-
-    trait :open do
-      after(:build) do |conversation|
-        conversation.update_attributes(state: :open)
-      end
-    end
-
-    trait :escalated do
-      after(:build) do |conversation|
-        conversation.update_attributes(state: :escalated)
-      end
-    end
   end
 end
