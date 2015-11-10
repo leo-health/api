8b1adc4d698892deb7ca7f8c41e4b40bcebde521
Wuang Qin <wuang@leohealth.com>
2015-11-10 12:04:47 -0500

    refactor the payload

--- app/notifications/apns_notification.rb
+++ app/notifications/apns_notification.rb
@@ -12,13 +12,11 @@ class ApnsNotification
     hello_notification = Grocer::Notification.new(
         device_token:      "63760a125ffeb4cc50f3e91f4a1ff609c4fb4df65aa4dd1453660d015cdff32e",
         alert:             "Hello!",
-        custom:            { url_host: "conversation" },
+        custom:            { url_host: "conversation", url_path: 0 },
         badge:             42,
-        category:          "a category",         # optional; used for custom notification actions
         sound:             "siren.aiff",         # optional
         expiry:            Time.now + 60*60,     # optional; 0 is default, meaning the message is not stored
-        identifier:        1234,                 # optional; must be an integer
-        content_available: true                  # optional; any truthy value will set 'content-available' to 1
+        content_available: false                  # optional; any truthy value will set 'content-available' to 1
     )
     pusher.push(hello_notification)
   end
