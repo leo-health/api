c5ba56b330d9ce67f229763e26ea93dfe31eaf05
Wuang Qin <wuang@leohealth.com>
2015-11-17 15:13:55 -0500

    use find_by_id in the mailer jobs

--- app/jobs/mail/invite_parent_job.rb
+++ app/jobs/mail/invite_parent_job.rb
@@ -1,7 +1,7 @@
 class InviteParentJob < Struct.new(:enrollment_id, :user_id)
   def perform
-    enrollment = Enrollment.try(:find, enrollment_id)
-    current_user = User.try(:find, user_id)
+    enrollment = Enrollment.find_by_id(enrollment_id)
+    current_user = User.find_by_id(user_id)
     UserMailer.invite_secondary_parent(enrollment, current_user).deliver if ( enrollment && current_user )
   end
 
--- app/jobs/mail/remind_schedule_appointment_job.rb
+++ app/jobs/mail/remind_schedule_appointment_job.rb
@@ -1,6 +1,6 @@
 class RemindScheduleAppointmentJob < Struct.new(:user_id)
   def perform
-    user = User.find_by_id(:user_id)
+    user = User.find_by_id(user_id)
     UserMailer.remind_schedule_appointment(user).deliver if user
   end
 
