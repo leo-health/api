8ee58ee3dc939d41443ef8803ec64212ae8e07b9
Wuang Qin <wuang@leohealth.com>
2015-12-11 12:04:53 -0500

    send provider a email about conversation escalated to them in off hours with high priority

--- app/models/escalation_note.rb
+++ app/models/escalation_note.rb
@@ -4,14 +4,19 @@ class EscalationNote < ActiveRecord::Base
 
   validates :escalated_by, :user_conversation, :priority, presence: true
 
-  after_commit :sms_provider, on: :create
+  after_commit :notify_provider, on: :create
 
   private
 
-  def sms_provider
+  def notify_provider
     staff = User.find(user_conversation.user_id)
-    if staff.practice.try(:in_office_hour?) || priority == 1
+    if staff.practice.try(:in_office_hour?)
       SendSmsJob.new(staff.id, 'A conversation has been escalated to you!').send
+    else
+      if priority == 1
+        UserMailer.delay.notify_escalated_conversation(staff)
+        SendSmsJob.new(staff.id, 'A conversation has been escalated to you!').send
+      end
     end
   end
 end
--- spec/models/escalation_note_spec.rb
+++ spec/models/escalation_note_spec.rb
@@ -18,10 +18,10 @@ RSpec.describe EscalationNote, type: :model do
     let!(:open_conversation){ create(:conversation, state: :open) }
     let(:escalation_params){ { escalated_to: clinical, note: 'escalation note', priority: 1, escalated_by: customer_service } }
 
-    context 'after commit' do
-      it "should send a sms notification to staff" do
+    describe 'after commit' do
+      it "should send a sms and email notification to staff" do
         expect( EscalationNote.count ).to eq(0)
-        expect{ open_conversation.escalate(escalation_params) }.to change(Delayed::Job, :count).by(1)
+        expect{ open_conversation.escalate(escalation_params) }.to change(Delayed::Job, :count).by(2)
         expect( EscalationNote.count ).to eq(1)
       end
     end
