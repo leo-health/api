917c33d6baf6c5f6c76b8e07acc9eb3d30c29041
Wuang Qin <wuang@leohealth.com>
2015-12-17 13:57:53 -0500

    user carrierwave-aws to replace fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,18 @@
-require 'fog/aws'
-
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     'AKIAIZJEJH6F6OQL43XQ',
-    aws_secret_access_key: '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',
-    region:                'us-east-1'
-  }
-
   if Rails.env.test? || Rails.env.cucumber?
-    config.storage           = :file
+    config.storage = :file
     config.enable_processing = false
-    config.root              = "#{Rails.root}/tmp"
+    config.root = "#{Rails.root}/tmp"
   else
-    config.storage = :fog
+    config.storage = :aws
   end
 
-  config.fog_directory  = 'leo-photos-development'
+  config.aws_bucket = 'leo-photos-development'
+  config.aws_acl    = 'public-read'
+
+  config.aws_credentials = {
+      access_key_id:     'AKIAIZJEJH6F6OQL43XQ',
+      secret_access_key: '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',
+      region:            'us-east-1'
+  }
 end
