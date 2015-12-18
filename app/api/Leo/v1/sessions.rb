a5e760c6c7aea17a600e984910b5d156352bb467
Wuang Qin <wuang@leohealth.com>
2015-12-18 15:48:44 -0500

    prevent bot user from login

--- app/api/Leo/v1/sessions.rb
+++ app/api/Leo/v1/sessions.rb
@@ -14,6 +14,7 @@ module Leo
         desc "create a session when user login"
         post do
           user = User.find_by_email(params[:email].downcase)
+          return if user.has_role? :bot
           unless user && user.valid_password?(params[:password])
             error!({error_code: 403, error_message: "Invalid Email or Password."}, 422)
           end
