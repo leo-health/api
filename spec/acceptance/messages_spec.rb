a884cada032d197ecb3244c8924e657c48c3402a
Wuang Qin <wuang@leohealth.com>
2015-12-22 12:56:44 -0500

    add example into racdoc for getting messages

--- spec/acceptance/messages_spec.rb
+++ spec/acceptance/messages_spec.rb
@@ -9,12 +9,12 @@ resource "Messages" do
   let(:session){ user.sessions.create }
   let(:authentication_token){ session.authentication_token }
   let(:message_params){{ body: "test", type_name: "text", sender: user, conversation: user.family.conversation }}
-  let(:message){ Message.create(message_params) }
 
   get "/api/v1/messages/:id" do
     parameter :id, "Message Id", :required => true
     parameter :authentication_token, "Authentication Token", :required => true
 
+    let(:message){ Message.create(message_params) }
     let(:id){ message.id }
     let(:raw_post){ params.to_json }
 
@@ -31,8 +31,19 @@ resource "Messages" do
     parameter :per_page, "Record Per Page (default 25)"
     parameter :offset, "The Offset to Start from (default: 0)"
 
+    let(:encoded_image){ Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'Zen-Dog1.png')) }
+    let(:photo_message_params){{ message_photo_attributes: { image: encoded_image },
+                                 type_name: "image",
+                                 sender: user,
+                                 conversation: user.family.conversation }}
+
     let(:conversation_id){ user.family.conversation.id }
     let(:raw_post){ params.to_json }
+
+    before do
+      Message.create(message_params)
+      Message.create(photo_message_params)
+    end
 
     example "get all messages of a conversation" do
       do_request
