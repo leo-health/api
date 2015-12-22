601e6b88add8fb04d63b1baf7d6b29ee58c45437
Wuang Qin <wuang@leohealth.com>
2015-12-22 11:51:56 -0500

    remove the non null constrains on database

--- db/migrate/20151222164750_remove_non_null_constrains_on_body_of_message.rb
+++ db/migrate/20151222164750_remove_non_null_constrains_on_body_of_message.rb
@@ -1,5 +1,9 @@
 class RemoveNonNullConstrainsOnBodyOfMessage < ActiveRecord::Migration
-  def change
-    change_column :messages, :body, :string, null: true
+  def up
+    change_column :messages, :body, :text, null: true
+  end
+
+  def down
+    change_column :messages, :body, :text, null: false
   end
 end
