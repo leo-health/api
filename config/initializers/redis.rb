2b0907575085d2e2c4ae585de2b0493033aaa380
Wuang Qin <wuang@leohealth.com>
2016-01-07 15:08:09 -0500

    add config for redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 if Rails.env.test?
   $redis = Redis.new
-else
+elsif Rails.env.development?
   $redis = Redis.new(:host => 'localhost', :port => 6379)
+else
+  $redis = Redis.new(:host => 'notificationcache.fhtgid.0001.use1.cache.amazonaws.com', :port => 6379)
 end
