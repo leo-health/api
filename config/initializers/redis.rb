b41738cc1fac1f7103a256b8d026f2d890fc367b
Misha Dragojevic <misha@cloudgurus.net>
2016-01-08 16:11:09 -0800

    Added Redis config for future POC

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,6 +2,8 @@ if Rails.env.test?
   $redis = Redis.new
 elsif Rails.env.development?
   $redis = Redis.new(:host => 'localhost', :port => 6379)
+elsif Rails.env.devserver?
+  $redis = Redis.new(:host => 'db-dev.leohealth.internal', :port => 6379)
 else
   $redis = Redis.new(:host => 'notificationcache.fhtgid.0001.use1.cache.amazonaws.com', :port => 6379)
 end
