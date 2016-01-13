c504063395c672b22687ecf8f5082e344788489b
Wuang Qin <wuang@leohealth.com>
2016-01-13 16:02:31 -0500

    remove no longer valid specs

--- spec/models/escalation_note_spec.rb
+++ spec/models/escalation_note_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe EscalationNote, type: :model do
   describe "relations" do
-    it{ is_expected.to belong_to(:user_conversation) }
+    it{ is_expected.to belong_to(:escalated_to) }
     it{ is_expected.to belong_to(:escalated_by).class_name('User') }
   end
 
   describe "validations" do
     it { is_expected.to validate_presence_of(:escalated_by) }
-    it { is_expected.to validate_presence_of(:user_conversation) }
+    it { is_expected.to validate_presence_of(:escalated_to) }
     it { is_expected.to validate_presence_of(:priority) }
   end
 
