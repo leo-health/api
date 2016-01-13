e0ff1fad4eaf0a775e13e25f265a433a35fa7880
Wuang Qin <wuang@leohealth.com>
2016-01-13 12:04:37 -0500

    add time zone to practice

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104181817) do
+ActiveRecord::Schema.define(version: 20160113170327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -419,6 +419,7 @@ ActiveRecord::Schema.define(version: 20160104181817) do
     t.string   "email"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "time_zone"
   end
 
   create_table "provider_additional_availabilities", force: :cascade do |t|
