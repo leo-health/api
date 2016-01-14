460d21f1594829fa3a08ddc83a24c0a7ee97c9f0
Wuang Qin <wuang@leohealth.com>
2016-01-14 14:48:59 -0500

    expose avatar and image as a filed named url

--- app/api/Leo/entities/avatar_entity.rb
+++ app/api/Leo/entities/avatar_entity.rb
@@ -2,7 +2,7 @@ module Leo
   module Entities
     class AvatarEntity < Grape::Entity
       expose :id
-      expose :avatar, with: Leo::Entities::ImageEntity
+      expose :avatar, with: Leo::Entities::ImageEntity, as: :url
       expose :owner_type
       expose :owner_id
       expose :created_at, as: :created_datetime
--- app/api/Leo/entities/form_entity.rb
+++ app/api/Leo/entities/form_entity.rb
@@ -4,7 +4,7 @@ module Leo
       expose :id, :title, :notes, :status
       expose :created_at, as: :created_datetime
       expose :updated_at, as: :updated_datetime
-      expose :image, with: Leo::Entities::ImageEntity
+      expose :image, with: Leo::Entities::ImageEntity, as: :url
       expose :status
       expose :patient, with: Leo::Entities::PatientEntity
       expose :submitted_by, with: Leo::Entities::UserEntity
