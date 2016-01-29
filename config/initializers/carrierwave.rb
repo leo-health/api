d2bfc72183d8198eacf2fbc1f98036190071afd9
Wuang Qin <wuang@leohealth.com>
2016-01-29 14:56:45 -0500

    set redis link, and carrierwave env variable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -25,11 +25,11 @@ CarrierWave.configure do |config|
 
   config.root = Rails.root.join 'tmp'
   config.aws_authenticated_url_expiration = 60 * 15
-  config.aws_bucket = ENV['carrierwave_aws_bucket']
+  config.aws_bucket = ENV['CARRIERWAVE_AWS_BUCKET']
 
   config.aws_credentials = {
-      access_key_id:     ENV['carrierwave_access_key_id'],
-      secret_access_key: ENV['carrierwave_secret_access_key'],
-      region:            ENV['carrierwave_region']
+      access_key_id:     ENV['CARRIERWAVE_ACCESS_KEY_ID'],
+      secret_access_key: ENV['CARRIERWAVE_SECRET_ACCESS_KEY'],
+      region:            ENV['CARRIERWAVE_REGION']
   }
 end
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,5 +1,5 @@
 MandrillMailer.configure do |config|
-  config.api_key = ENV["mandrill_api_key"]
+  config.api_key = ENV["MANDRILL_API_KEY"]
   config.deliver_later_queue_name = :default
 
   if %w(test, development).any?{|e|e == Rails.env}
