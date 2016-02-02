752bb59e93931d72f2542ba663682fedc5591a58
Wuang Qin <wuang@leohealth.com>
2016-02-02 12:15:00 -0500

    set up staff_profile model

--- app/models/staff_profile.rb
+++ app/models/staff_profile.rb
@@ -1,10 +1,5 @@
 class StaffProfile < ActiveRecord::Base
-  belongs_to :staff, ->{where('role_id != ?', 4)}, foreign_key: "user_id", class_name: "User"
+  belongs_to :staff, ->{where('role_id != ?', 4)}, class_name: "User"
 
   validates :staff, presence: true
-  # validate :provider_identity
-
-  # def provider_identity
-  #   errors.add(:provider_id, "must be a provider") unless provider.has_role? :clinical
-  # end
 end
