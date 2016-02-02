c47be2c9967e5de65192e41faa22a9905f1217ae
Wuang Qin <wuang@leohealth.com>
2016-02-02 12:23:38 -0500

    add spec for staff_profile model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,7 @@ describe User do
     it{ is_expected.to belong_to(:practice) }
     it{ is_expected.to belong_to(:onboarding_group) }
 
-    it{ is_expected.to have_one(:provider_profile).with_foreign_key('provider_id') }
+    it{ is_expected.to have_one(:staff_profile).with_foreign_key('staff_id') }
 
     it{ is_expected.to have_many(:user_conversations) }
     it{ is_expected.to have_many(:forms) }
