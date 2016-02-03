c0cb7456279cb6db001f853090458a5432e47071
Davor Mrkoci <davormrkoci@gmail.com>
2016-02-03 11:36:38 -0500

    Set the production limit to 100 requests/sec

--- config/initializers/athena_health_api.rb
+++ config/initializers/athena_health_api.rb
@@ -2,7 +2,7 @@ require 'athena_health_api'
 
 AthenaHealthAPI.configure do |config|
   if Rails.env.production?
-    config.min_request_interval = (0.000002).seconds
+    config.min_request_interval = (0.01).seconds
   else
     config.min_request_interval = (0.2).seconds
   end
