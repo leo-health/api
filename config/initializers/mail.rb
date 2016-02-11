8df19c1186367cffd0d7b79cd489f0de84d83ef1
Wuang Qin <wuang@leohealth.com>
2016-02-10 19:11:03 -0500

    intercept every email except those go to leohealth.com

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,12 +2,11 @@ MandrillMailer.configure do |config|
   config.api_key = ENV["MANDRILL_API_KEY"]
   config.deliver_later_queue_name = :default
 
-  if %w(test, development).any?{|e|e == Rails.env}
+  if %w(test development develop).any?{|e|e == Rails.env}
     config.interceptor = Proc.new {|params|
-      params[:to] =  [
-        params[:to],
-        { email: "mailer@leohealth.com", name: "Wuang"}
-      ].flatten
+      unless params["to"].split("@").last == "leohealth.com"
+        params[:to] =  [params[:to], { email: "mailer@leohealth.com", name: "Wuang"}].flatten
+      end
     }
   end
 end
