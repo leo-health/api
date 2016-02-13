27fbfcf75920b551f7556d36fb2118a7f1019e74
Davor Mrkoci <davormrkoci@gmail.com>
2016-02-12 21:11:01 -0500

    Mark appointment as cancelled rather then deleting.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -12,7 +12,8 @@ class Appointment < ActiveRecord::Base
             :appointment_type, :booked_by, :provider, :patient, :practice, presence: true
 
   validate :same_family?, on: :create
-  validates_uniqueness_of :start_datetime, scope: :provider_id, conditions: -> { where(deleted_at: nil)}
+  validates_uniqueness_of :start_datetime, scope: :provider_id, 
+    conditions: -> { where(deleted_at: nil, appointment_status: AppointmentStatus.booked) }
 
   def same_family?
     return unless (patient && booked_by)
@@ -33,26 +34,26 @@ class Appointment < ActiveRecord::Base
   end
 
   def cancelled?
-    appointment_status.status == "x"
+    appointment_status.status == AppointmentStatus::STATUS_CANCELLED
   end
 
   def future?
-    appointment_status.status == "f"
+    appointment_status.status == AppointmentStatus::STATUS_FUTURE
   end
 
   def open?
-    appointment_status.status == "o"
+    appointment_status.status == AppointmentStatus::STATUS_OPEN
   end
 
   def checked_in?
-    appointment_status.status == "2"
+    appointment_status.status == AppointmentStatus::STATUS_CHECKED_IN
   end
 
   def checked_out?
-    appointment_status.status == "3"
+    appointment_status.status == AppointmentStatus::STATUS_CHECKED_OUT
   end
 
   def charge_entered?
-    appointment_status.status == "4"
+    appointment_status.status == AppointmentStatus::STATUS_CHARGE_ENTERED
   end
 end
--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,7 +1,7 @@
 class Practice < ActiveRecord::Base
   has_many :staff, ->{ where role_id: [2, 3, 5] }, class_name: "User"
   has_many :guardians, ->{ where role_id: 4 }, class_name: "User"
-  has_many :appointments
+  has_many :appointments, -> { where appointment_status: AppointmentStatus.booked }
   has_many :practice_schedules
 
   validates :name, presence: true
