0b2df7374edd8563d8c50d94d5f363cdde5f887c
Wuang Qin <wuang@leohealth.com>
2016-02-16 11:14:06 -0500

    set up the route for rspec api doc generator

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   require_relative '../app/api/Leo/root'
 
   mount Leo::Root => '/'
-#  mount Raddocs::App => "/docs"
+  mount Raddocs::App => "/docs"
 
   root 'home#index'
 end
