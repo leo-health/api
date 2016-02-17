64e41a8d645f50458cd87ee71e66b9f4d29dfbd4
Wuang Qin <wuang@leohealth.com>
2016-02-17 13:58:42 -0500

    mount docs routes only for develop and development env

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   require_relative '../app/api/Leo/root'
 
   mount Leo::Root => '/'
-  mount Raddocs::App => "/docs"
+
+  if Rails.env.development? || Rails.env.develop?
+   mount Raddocs::App => "/docs"
+  end
 
   root 'home#index'
 end
