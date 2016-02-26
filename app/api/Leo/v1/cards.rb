55e566c6a1731abc418b0b87b8d2fff68cc26aad
Wuang Qin <wuang@leohealth.com>
2016-02-26 11:33:20 -0500

    remove past appointments

--- app/api/Leo/v1/cards.rb
+++ app/api/Leo/v1/cards.rb
@@ -9,13 +9,9 @@ module Leo
 
         get do
           family = Family.includes(:guardians).find(current_user.family_id)
-          appointments = Appointment.includes(:appointment_status)
-                           .where( booked_by_id: family.guardians.pluck(:id),
-                                   appointment_status: AppointmentStatus.existing)
-                           .order("created_at DESC")
-
-          upcoming_appointments, past_appointment = appointments.partition{|appointment| appointment.start_datetime.utc > Time.now}
-          cards = (upcoming_appointments + [family.conversation] + past_appointment).each_with_index.inject([]) do |cards, (card, index)|
+          appointments = Appointment.booked.where( booked_by_id: family.guardians.pluck(:id) )
+                           .where("start_datetime > ?", Time.now).order("created_at DESC")
+          cards = (appointments + [family.conversation]).each_with_index.inject([]) do |cards, (card, index)|
             case card
             when Conversation
               cards << {conversation_card_data: card, priority: index, type: 'conversation', type_id: 1}
@@ -23,6 +19,7 @@ module Leo
               cards << {appointment_card_data: card, priority: index, type: 'appointment', type_id: 0}
             end
           end
+
           present cards, with: Leo::Entities::CardEntity
         end
       end
