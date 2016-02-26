7edaf8e94a5a935ea3ddd26d88ebd8733ac89f3f
Wuang Qin <wuang@leohealth.com>
2016-02-26 13:25:22 -0500

    sort the cards by the time updated

--- app/api/Leo/v1/cards.rb
+++ app/api/Leo/v1/cards.rb
@@ -10,8 +10,10 @@ module Leo
         get do
           family = Family.includes(:guardians).find(current_user.family_id)
           appointments = Appointment.booked.where( booked_by_id: family.guardians.pluck(:id) )
-                           .where("start_datetime > ?", Time.now).order("created_at DESC")
-          cards = (appointments + [family.conversation]).each_with_index.inject([]) do |cards, (card, index)|
+                           .where("start_datetime > ?", Time.now).order("updated_at DESC")
+
+          cards = sort_cards(appointments + [family.conversation])
+          sorted_cards = cards.each_with_index.inject([]) do |cards, (card, index)|
             case card
             when Conversation
               cards << {conversation_card_data: card, priority: index, type: 'conversation', type_id: 1}
@@ -20,7 +22,29 @@ module Leo
             end
           end
 
-          present cards, with: Leo::Entities::CardEntity
+          present sorted_cards, with: Leo::Entities::CardEntity
+        end
+      end
+
+      helpers do
+        def sort_cards(cards)
+          count = cards.count
+          return cards if cards.count < 2
+          left, right = cards.take(count/2), cards.drop(count/2)
+          sorted_left, sorted_right = sort_cards(left), sort_cards(right)
+          merge(sorted_left, sorted_right)
+        end
+
+        def merge(left, right)
+          result = []
+          while left.count > 0 && right.count > 0
+            if left.first.updated_at >= right.first.updated_at
+              result << left.shift
+            else
+              result << right.shift
+            end
+          end
+          result + left + right
         end
       end
     end
