3f35af33a14658a2ac41174a3f01a4a5a4fe9b53
Davor Mrkoci <davormrkoci@gmail.com>
2016-03-11 13:24:00 -0500

    attempt to sync reschedule appointment right away.  Remove last modified date as it seems to cause missed appointments.

--- app/helpers/sync_service_helper.rb
+++ app/helpers/sync_service_helper.rb
@@ -154,16 +154,12 @@ module SyncServiceHelper
       #mm/dd/yyyy hh:mi:ss
       start_date = nil
       start_date = DateTime.parse(sync_params[:start_date.to_s]) if sync_params[:start_date.to_s]
-      start_last_modified = nil
-      start_last_modified = start_date.strftime("%m/%d/%Y %H:%M:00") if start_date
       next_start_date = DateTime.now.to_s
 
       booked_appts = @connector.get_booked_appointments(
         departmentid: task.sync_id,
         startdate: 1.year.ago.strftime("%m/%d/%Y"),
-        enddate: 1.year.from_now.strftime("%m/%d/%Y"),
-        startlastmodified: start_last_modified
-      )
+        enddate: 1.year.from_now.strftime("%m/%d/%Y"))
 
       booked_appts.each { |appt|
         leo_appt = Appointment.find_by(athena_id: appt.appointmentid.to_i)
@@ -305,6 +301,13 @@ module SyncServiceHelper
         #attempt to find rescheduled appt.  If not found, it will get updated on the next run.
         if (athena_appt.respond_to? :rescheduledappointmentid) && (athena_appt.rescheduledappointmentid.to_i != 0)
           rescheduled_appt = Appointment.find_by(athena_id: athena_appt.rescheduledappointmentid.to_i)
+
+          unless rescheduled_appt
+            #sync rescheduled appointment
+            rescheduled_athena_appt = @connector.get_appointment(appointmentid: leo_appt.athena_id)
+            rescheduled_appt = impl_create_leo_appt_from_athena(rescheduled_athena_appt) if rescheduled_athena_appt
+          end
+
           leo_appt.rescheduled_id = rescheduled_appt.id if rescheduled_appt
         end
       end
