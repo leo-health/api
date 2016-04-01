b29cb8733551630ef8540ea31fe9539ccd60fa5a
afanslau <afanslau@gmail.com>
2016-03-31 20:14:01 -0400

    Revert "543 - Fix: reload patient record after force sync"
    This reverts commit 6bb0e2b51cf409c29467c6b91ff6b100ec97aa08.

--- app/models/sync_task.rb
+++ app/models/sync_task.rb
@@ -1,20 +1,7 @@
 class SyncTask < ActiveRecord::Base
 
-  @@max_queue_position = SyncTask.maximum(:queue_position)
-  def self.max_queue_position
-    @@max_queue_position
-  end
-
   def enqueue
-    @@max_queue_position += 1
-    update_attributes(queue_position: @@max_queue_position)
-    if @@max_queue_position > 10000
-      SyncTask.transaction do
-        SyncTask.all.find_each.with_index do |sync_task, index|
-          sync_task.update_attributes(queue_position: index)
-        end
-      end
-    end
+    self.queue_position = SyncTask.maximum(:queue_position) + 1
     self
   end
 
