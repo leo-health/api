1536068d2aea658aab6bb03161b4371d39908ceb
Wuang Qin <wuang@leohealth.com>
2016-04-05 12:49:15 -0400

    chagne the AddVendorIdToEnrollmentAndUser migration

--- db/migrate/20160401182604_add_vendor_id_to_enrollment_and_user.rb
+++ db/migrate/20160401182604_add_vendor_id_to_enrollment_and_user.rb
@@ -1,8 +1,14 @@
 class AddVendorIdToEnrollmentAndUser < ActiveRecord::Migration
-  def change
+  def up
     add_column :users, :vendor_id, :string
-    add_column :enrollments, :vendor_id, :string, null: false
+    add_column :enrollments, :vendor_id, :string
+    change_column_null :enrollments, :vendor_id, false
     add_index :users, :vendor_id, unique: true
     add_index :enrollments, :vendor_id, unique: true
+  end
+
+  def down
+    remove_column :users, :vendor_id, :string
+    remove_column :enrollments, :vendor_id, :string
   end
 end
