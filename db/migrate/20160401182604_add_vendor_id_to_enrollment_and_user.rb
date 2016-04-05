df4253d11837dc16062c3ef509b081a334819c1f
Wuang Qin <wuang@leohealth.com>
2016-04-05 14:00:05 -0400

    seperate the add null false migration

--- db/migrate/20160401182604_add_vendor_id_to_enrollment_and_user.rb
+++ db/migrate/20160401182604_add_vendor_id_to_enrollment_and_user.rb
@@ -2,7 +2,6 @@ class AddVendorIdToEnrollmentAndUser < ActiveRecord::Migration
   def up
     add_column :users, :vendor_id, :string
     add_column :enrollments, :vendor_id, :string
-    change_column_null :enrollments, :vendor_id, false
     add_index :users, :vendor_id, unique: true
     add_index :enrollments, :vendor_id, unique: true
   end
