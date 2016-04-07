efeb1f5c2e48ca9a5b6862b99ec8ccd839c1afbc
Wuang Qin <wuang@leohealth.com>
2016-04-07 13:47:24 -0400

    add specs for the rate limiter

--- app/helpers/athena_health_api.rb
+++ app/helpers/athena_health_api.rb
@@ -260,4 +260,52 @@ module AthenaHealthAPI
       call(request, {}, headers, false, ignore_throttle)
     end
   end
+
+  class RateLimiter
+    attr_reader :athena_api_key, :per_second_rate_limit, :per_day_rate_limit
+
+    def initialize( per_second_rate_limit = 5, per_day_rate_limit = 50000, athena_api_key)
+      @per_day_rate_limit = per_day_rate_limit
+      @per_second_rate_limit = per_second_rate_limit
+      @athena_api_key = athena_api_key
+    end
+
+    def sleep_time
+      [sleep_time_day_rate_limit, sleep_time_second_rate_limit].max
+    end
+
+    def sleep_time_day_rate_limit
+      key = day_key
+      count, _ = $redis.multi do
+        $redis.incr(key)
+        $redis.expireat(key, @next_day)
+      end
+      count < per_day_rate_limit ? 0 : @next_day - Time.now.to_i
+    end
+
+    def sleep_time_second_rate_limit
+      count, _ = $redis.multi do
+        $redis.incr(second_key)
+        $redis.expire(second_key, 1)
+      end
+      count < per_second_rate_limit ? 0 : 1
+    end
+
+    private
+
+    def day_key
+      if Time.now.to_i <=  $redis.get('expire_at').to_i
+        "day_rate_limit:#{athena_api_key}:#{$redis.get('expire_at')}"
+      else
+        @next_day = (Time.now + 1.day).to_i
+        "day_rate_limit:#{athena_api_key}:#{@next_day.to_s}"
+      end
+    end
+
+    def second_key
+      time_pattern = Time.now.strftime("%Y-%m-%d-%H-%M-%S")
+      "second_rate_limit:#{athena_api_key}:#{time_pattern}"
+    end
+  end
+
 end
--- spec/helpers/athena_health_api_spec.rb
+++ spec/helpers/athena_health_api_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'athena_health_api'
 
-RSpec.describe AthenaHealthAPI, type: :helper do
+describe AthenaHealthAPI do
   describe "Athena Health Api - " do
     let(:connection) { double("connection") }
     let(:api_connection) { AthenaHealthAPI::Connection.new("version", "key", "secret", "practice_id") }
@@ -50,6 +50,73 @@ RSpec.describe AthenaHealthAPI, type: :helper do
         end
 
         expect(Time.now - start_time).to be < AthenaHealthAPI.configuration.effective_min_request_interval
+      end
+    end
+  end
+
+  describe "RateLimiter" do
+    subject { AthenaHealthAPI::RateLimiter.new("athena_key") }
+
+    describe "sleep_time_day_rate_limit" do
+      context "under day rate limit" do
+        it "should return 0 sleep time" do
+          expect(subject.sleep_time_day_rate_limit).to eq(0)
+        end
+      end
+
+      context "over day rate limit" do
+        let(:key){ "day_rate_limit:#{subject.athena_api_key}:#{(Time.now + 1.day).to_i.to_s}" }
+
+        before do
+          Timecop.freeze(Time.now)
+          $redis.set(key, subject.per_day_rate_limit)
+        end
+
+        after do
+          Timecop.return
+        end
+
+        it "should return time left till the 24hour cycle end" do
+          expect(subject.sleep_time_day_rate_limit).to eq( 24*60*60 )
+        end
+      end
+
+      context "over day rate limit, and pass time cycle" do
+        let(:key){ "day_rate_limit:#{subject.athena_api_key}:#{(Time.now + 1.day).to_i.to_s}" }
+
+        before do
+          Timecop.freeze(Time.now)
+          $redis.set(key, subject.per_day_rate_limit)
+        end
+
+        after do
+          Timecop.return
+        end
+      end
+    end
+
+    describe "#sleep_time_second_rate_limit" do
+      context "under second rate limit" do
+        it "should return 0 sleep time" do
+          expect(subject.sleep_time_second_rate_limit).to eq(0)
+        end
+      end
+
+      context "over second rate limit" do
+        let(:key){ "second_rate_limit:#{subject.athena_api_key}:#{Time.now.strftime("%Y-%m-%d-%H-%M-%S")}" }
+
+        before do
+          Timecop.freeze(Time.now)
+          $redis.set(key, subject.per_second_rate_limit)
+        end
+
+        after do
+          Timecop.return
+        end
+
+        it "should return 1 second of sleep time" do
+          expect(subject.sleep_time_second_rate_limit).to eq( 1 )
+        end
       end
     end
   end
