f4a892ddbb54749e1b56b41356e5248fc4b20c86
afanslau <afanslau@gmail.com>
2016-04-14 10:40:42 -0400

    add person model

--- app/models/provider_sync_profile.rb
+++ app/models/provider_sync_profile.rb
@@ -1,6 +1,9 @@
 class ProviderSyncProfile < ActiveRecord::Base
   belongs_to :provider, ->{ provider }, class_name: "User"
 
+  belongs_to :practice
+  belongs_to :person
+  accepts_nested_attributes_for :person
   validates_uniqueness_of :athena_id, conditions: ->{ where.not(athena_id: 0) }
 
   after_commit :subscribe_to_athena, on: :create
