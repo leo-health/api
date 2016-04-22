2b36dee718658af60e35af981446e7461bb3bf13
afanslau <afanslau@gmail.com>
2016-04-22 09:54:59 -0400

    Dont depend on Roles in the DB to define RoleCheckable methods

--- app/models/concerns/role_checkable.rb
+++ app/models/concerns/role_checkable.rb
@@ -9,17 +9,18 @@ module RoleCheckable
   def provider?
     clinical?
   end
-  
+
   private
   def role_by_name?
     role.try(:name) && __callee__.to_s == role.name + "?"
   end
 
   included do
-    Role.find_each do |role|
-      check_method_name = (role.name.to_s + "?").to_sym
-      alias_method check_method_name, :role_by_name?
-      public check_method_name
+    roles = Role.all.map(&:name) | [:financial, :clinical_support, :customer_service, :guardian, :clinical, :bot, :operational].map(&:to_s)
+    check_method_names = roles.map { |role| (role + "?").to_sym }
+    check_method_names.each do |role_name|
+      alias_method role_name, :role_by_name?
+      public role_name
     end
   end
 end
