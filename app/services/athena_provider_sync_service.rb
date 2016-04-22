4ad2832cd499903056c9bbcd9b9b23d5370943c7
afanslau <afanslau@gmail.com>
2016-04-22 19:13:38 -0400

    Update provider service to use 6 months of frozen slots

--- app/services/athena_provider_sync_service.rb
+++ app/services/athena_provider_sync_service.rb
@@ -1,50 +1,25 @@
 class AthenaProviderSyncService < AthenaSyncService
   def sync_provider_leave(provider_sync_profile)
+    unavailable_appts_for_provider = @connector.get_open_appointments(
+      departmentid: provider_sync_profile.athena_department_id,
+      appointmenttypeid: 0,
+      providerid: provider_sync_profile.athena_id,
+      showfrozenslots: true,
+      startdate: Date.today.strftime("%m/%d/%Y"),
+      enddate: (Date.today + 6.months).strftime("%m/%d/%Y")
+    ).select { |appt| (appt.try(:frozen) == "true" && appt.providerid.to_i == provider_sync_profile.athena_id) }
 
-    blocked_appointment_type = AppointmentType.find_by!(name: "Block")
-
-    blocked_appts = @connector.get_open_appointments(
-    departmentid: provider_sync_profile.athena_department_id,
-    appointmenttypeid: blocked_appointment_type.athena_id,
-    providerid: provider_sync_profile.athena_id
-    ).select {|appt| (appt.appointmenttypeid.to_i == blocked_appointment_type.athena_id && appt.providerid.to_i == provider_sync_profile.athena_id) }
-
-    frozen_appts = @connector.get_open_appointments(
-    departmentid: provider_sync_profile.athena_department_id,
-    appointmenttypeid: 0,
-    providerid: provider_sync_profile.athena_id,
-    showfrozenslots: true
-    ).select {|appt| (appt.try(:frozen) == "true" && appt.providerid.to_i == provider_sync_profile.athena_id) }
-
-    #delete all existing provider leaves that are synched from athena
     ProviderLeave.where(athena_provider_id: provider_sync_profile.athena_id).where.not(athena_id: 0).destroy_all
-
-    #add new entries
-    blocked_appts.each { |appt|
+    provider_sync_profile.update(leave_updated_at: DateTime.now.utc) if unavailable_appts_for_provider.size
+    unavailable_appts_for_provider.map { |appt|
       start_datetime = AthenaHealthApiHelper.to_datetime(appt.date, appt.starttime)
-
       ProviderLeave.create(
-      athena_id: appt.appointmentid.to_i,
-      athena_provider_id: appt.providerid.to_i,
-      description: "Synced from Athena block.id=#{appt.appointmentid}",
-      start_datetime: start_datetime,
-      end_datetime: start_datetime + appt.duration.to_i.minutes
+        athena_id: appt.appointmentid.to_i,
+        athena_provider_id: appt.providerid.to_i,
+        description: "Synced from Athena block.id=#{appt.appointmentid}",
+        start_datetime: start_datetime,
+        end_datetime: start_datetime + appt.duration.to_i.minutes
       )
     }
-
-    frozen_appts.each { |appt|
-      start_datetime = AthenaHealthApiHelper.to_datetime(appt.date, appt.starttime)
-
-      ProviderLeave.create(
-      athena_id: appt.appointmentid.to_i,
-      athena_provider_id: appt.providerid.to_i,
-      description: "Synced from Athena block.id=#{appt.appointmentid}",
-      start_datetime: start_datetime,
-      end_datetime: start_datetime + appt.duration.to_i.minutes
-      )
-    }
-
-    provider_sync_profile.leave_updated_at = DateTime.now.utc
-    provider_sync_profile.save!
   end
 end
