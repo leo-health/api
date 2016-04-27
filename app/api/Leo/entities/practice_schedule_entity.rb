27911610ca02793db4c8b611d113a786d0e1353b
Nayan Jain <nayan@leohealth.com>
2016-04-27 17:26:41 -0400

    No issue - Address PR comments for spaces

--- app/api/Leo/entities/practice_schedule_entity.rb
+++ app/api/Leo/entities/practice_schedule_entity.rb
@@ -13,31 +13,31 @@ module Leo
       private
       #suboptimal
       def monday
-      	{ id: 1, start_time: object.monday_start_time, end_time: object.monday_end_time }
+        { id: 1, start_time: object.monday_start_time, end_time: object.monday_end_time }
       end
 
       def tuesday
-      	{ id: 2, start_time: object.tuesday_start_time, end_time: object.tuesday_end_time }
+        { id: 2, start_time: object.tuesday_start_time, end_time: object.tuesday_end_time }
       end
 
       def wednesday
-      	{ id: 3, start_time: object.wednesday_start_time, end_time: object.wednesday_end_time }
+        { id: 3, start_time: object.wednesday_start_time, end_time: object.wednesday_end_time }
       end
 
       def thursday
-      	{ id: 4, start_time: object.thursday_start_time, end_time: object.thursday_end_time }
+        { id: 4, start_time: object.thursday_start_time, end_time: object.thursday_end_time }
       end
 
       def friday
-      	{ id: 5, start_time: object.friday_start_time, end_time: object.friday_end_time }
+        { id: 5, start_time: object.friday_start_time, end_time: object.friday_end_time }
       end
 
       def saturday
-      	{ id: 6, start_time: object.saturday_start_time, end_time: "23:59" }
+        { id: 6, start_time: object.saturday_start_time, end_time: "23:59" }
       end
 
       def sunday
-      	{ id: 7, start_time: object.sunday_start_time, end_time: "23:59" }
+        { id: 7, start_time: object.sunday_start_time, end_time: "23:59" }
       end
     end
   end
