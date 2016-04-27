faeccf0365a215b0cb466fa397dbe0686b8486af
Nayan Jain <nayan@leohealth.com>
2016-04-27 15:10:24 -0400

    No issue - Tabs to spaces

--- app/api/Leo/entities/practice_schedule_entity.rb
+++ app/api/Leo/entities/practice_schedule_entity.rb
@@ -1,44 +1,44 @@
 module Leo
   module Entities
     class PracticeScheduleEntity < Grape::Entity
-		expose :schedule_type
-		expose :monday
-		expose :tuesday
-		expose :wednesday
-		expose :thursday
-		expose :friday
-		expose :saturday
-		expose :sunday
+      expose :schedule_type
+      expose :monday
+      expose :tuesday
+      expose :wednesday
+      expose :thursday
+      expose :friday
+      expose :saturday
+      expose :sunday
 
-		private
-		#suboptimal
-		def monday
-			{ id: 1, start_time: object.monday_start_time, end_time: object.monday_end_time }
-		end
+      private
+      #suboptimal
+      def monday
+      	{ id: 1, start_time: object.monday_start_time, end_time: object.monday_end_time }
+      end
 
-		def tuesday
-			{ id: 2, start_time: object.tuesday_start_time, end_time: object.tuesday_end_time }
-		end
+      def tuesday
+      	{ id: 2, start_time: object.tuesday_start_time, end_time: object.tuesday_end_time }
+      end
 
-		def wednesday
-			{ id: 3, start_time: object.wednesday_start_time, end_time: object.wednesday_end_time }
-		end
+      def wednesday
+      	{ id: 3, start_time: object.wednesday_start_time, end_time: object.wednesday_end_time }
+      end
 
-		def thursday
-			{ id: 4, start_time: object.thursday_start_time, end_time: object.thursday_end_time }
-		end
+      def thursday
+      	{ id: 4, start_time: object.thursday_start_time, end_time: object.thursday_end_time }
+      end
 
-		def friday
-			{ id: 5, start_time: object.friday_start_time, end_time: object.friday_end_time }
-		end
+      def friday
+      	{ id: 5, start_time: object.friday_start_time, end_time: object.friday_end_time }
+      end
 
-		def saturday
-			{ id: 6, start_time: object.saturday_start_time, end_time: "23:59" }
-		end
+      def saturday
+      	{ id: 6, start_time: object.saturday_start_time, end_time: "23:59" }
+      end
 
-		def sunday
-			{ id: 7, start_time: object.sunday_start_time, end_time: "23:59" }
-		end
+      def sunday
+      	{ id: 7, start_time: object.sunday_start_time, end_time: "23:59" }
+      end
     end
   end
 end
