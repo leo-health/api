22197eb1ff3c200f0fe370ef6f29b61dbdde49e7
afanslau <afanslau@gmail.com>
2016-05-03 12:22:29 -0400

    Fix: make drop_table reversible

--- db/migrate/20160412192726_remove_unused_sync_columns.rb
+++ db/migrate/20160412192726_remove_unused_sync_columns.rb
@@ -12,7 +12,21 @@ class RemoveUnusedSyncColumns < ActiveRecord::Migration
   end
 
   def self.down
-    create_table :sync_tasks
+    create_table "sync_tasks", force: :cascade do |t|
+      t.integer  "sync_id",     default: 0,     null: false
+      t.string   "sync_type",   default: "",    null: false
+      t.datetime "created_at",                  null: false
+      t.datetime "updated_at",                  null: false
+      t.string   "sync_params", default: "",    null: false
+      t.integer  "num_failed",  default: 0,     null: false
+      t.boolean  "working",     default: false, null: false
+    end
+
+    add_index "sync_tasks", ["num_failed"], name: "index_sync_tasks_on_num_failed", using: :btree
+    add_index "sync_tasks", ["sync_id"], name: "index_sync_tasks_on_sync_id", using: :btree
+    add_index "sync_tasks", ["sync_type"], name: "index_sync_tasks_on_sync_type", using: :btree
+    add_index "sync_tasks", ["working"], name: "index_sync_tasks_on_working", using: :btree
+    
     add_column :delayed_jobs, :cron, :string
     add_reference :patients, :sync_job, index: true, references: :delayed_jobs
     add_reference :patients, :vitals_sync_job, index: true, references: :delayed_jobs
