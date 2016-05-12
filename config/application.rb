80c044da482131fde283745bc3947a3ec7d1a550
afanslau <afanslau@gmail.com>
2016-05-11 20:05:32 -0400

    Fix: Change concerns to mixins

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ module Api
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', '*')]
     config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
     config.autoload_paths += Dir[Rails.root.join('app', 'jobs', '*')]
     config.active_job.queue_adapter = :delayed_job
