dfa7cec8d8e2021d9d2d6bb3503b07552f354688
afanslau <afanslau@gmail.com>
2016-06-01 15:34:49 -0400

    Feature: copy enrollments/invite to family/invite, update guardians to only include complete users

--- spec/requests/families_spec.rb
+++ spec/requests/families_spec.rb
@@ -21,4 +21,24 @@ describe Leo::V1::Families do
       expect(body[:data][:family].as_json.to_json).to eq(serializer.represent(user.reload.family).as_json.to_json)
     end
   end
+
+  describe "POST /api/v1/family/invite" do
+    let!(:user){ create(:user, :guardian) }
+    let(:session){ user.sessions.create }
+    let!(:onboarding_group){ create(:onboarding_group)}
+
+    def do_request
+      enrollment_params = { email: "wuang@leohealth.com", first_name: "Yellow", last_name: "BigRiver" }
+      enrollment_params.merge!(authentication_token: session.authentication_token)
+      post "/api/v1/family/invite", enrollment_params
+    end
+
+    it "should send a invite to the user" do
+      expect{ do_request }.to change{ Delayed::Job.count }.by(1)
+      expect(response.status).to eq(201)
+      body = JSON.parse(response.body, symbolize_names: true )
+      expect(body[:data][:onboarding_group].to_sym).to eq(:invited_secondary_guardian)
+      expect(user.family.reload.guardians.count).to be(1)
+    end
+  end
 end
