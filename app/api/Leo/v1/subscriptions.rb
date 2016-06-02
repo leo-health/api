23a11b0ec2992124c06f8e6417a320d90978eafb
afanslau <afanslau@gmail.com>
2016-06-02 14:52:27 -0400

    Fix: log entire error from stripe to fix no method error for `[]`

--- app/api/Leo/v1/subscriptions.rb
+++ app/api/Leo/v1/subscriptions.rb
@@ -26,13 +26,13 @@ module Leo
           begin
             family.update_or_create_stripe_subscription_if_needed! params[:credit_card_token]
           rescue Stripe::AuthenticationError => e
-            error!({error_code: 500, error_message: "Stripe::AuthenticationError #{e.json_body[:error][:code]}" }, 500)
+            error!({error_code: 500, error_message: "Stripe::AuthenticationError #{e.to_s}" }, 500)
           rescue Stripe::CardError => e
             family.expire_membership!
             error!({error_code: 422, error_message: "Your card was declined, please try a different one." }, 422)
-            logger.error("#{user.email}: #{e.json_body[:error][:message]}")
+            logger.error("#{user.email}: #{e.to_s}")
           rescue Stripe::RateLimitError, Stripe::APIConnectionError => e
-            error!({error_code: 500, error_message: "Stripe::RateLimitError APIConnectionError #{e.json_body[:error][:code]}" }, 500)
+            error!({error_code: 500, error_message: "Stripe::RateLimitError APIConnectionError #{e.to_s}" }, 500)
           rescue Stripe::StripeError => e
             error!({error_code: 500, error_message: "Stripe::StripeError #{e.to_s}" }, 500)
             logger.error("#{user.email}: #{e.to_s}")
