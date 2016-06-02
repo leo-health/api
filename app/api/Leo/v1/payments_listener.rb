f403447d2d3ce835cc39f7214c5be1f7ac141ecd
afanslau <afanslau@gmail.com>
2016-06-02 11:13:06 -0400

    645 - Feature: only expire membership if next_payment_attempt is null

--- app/api/Leo/v1/payments_listener.rb
+++ app/api/Leo/v1/payments_listener.rb
@@ -6,10 +6,10 @@ module Leo
           event_type = params["type"]
           related_object = params["data"]["object"]
           case event_type
-          when "charge.failed"
+          when "invoice.failed"
             customer_id = related_object["customer"]
             if family = Family.find_by_stripe_customer_id(customer_id)
-              family.expire_membership!
+              family.expire_membership! unless related_object["next_payment_attempt"]
               PaymentsMailer.invalid_payment_method family
             end
           end
