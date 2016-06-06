c521fdc2a195f1f51f8a192a7fa2f7826b2e8313
afanslau <afanslau@gmail.com>
2016-06-06 17:56:20 -0400

    647 - Code Review: tests and break down large methods

--- app/models/family.rb
+++ app/models/family.rb
@@ -18,7 +18,7 @@ class Family < ActiveRecord::Base
       after do
         patients.map(&:subscribe_to_athena)
       end
-      
+
       transitions from: [:incomplete, :delinquent], to: :member
     end
 
@@ -40,31 +40,42 @@ class Family < ActiveRecord::Base
   end
 
   def stripe_customer=(stripe_customer)
-    limited_stripe_customer = {
-      id: stripe_customer[:id]
-    }
+    limited_stripe_customer = parse_limited_stripe_customer(stripe_customer)
+    super limited_stripe_customer
+    self.stripe_customer_id = limited_stripe_customer[:id]
+  end
 
-    limited_stripe_subscription = GenericHelper.try_nested_value_for_key_path(
-      stripe_customer,
-      [:subscriptions, :data, 0]
-    ).try(:slice, :id, :quantity)
+  private
 
-    if limited_stripe_subscription
-      limited_stripe_subscription[:plan] = GenericHelper.try_nested_value_for_key_path(
-        stripe_customer,
-        [:subscriptions, :data, 0, :plan]
-      ).try(:slice, :id, :amount)
-
+  def parse_limited_stripe_customer(full_stripe_customer)
+    # Limit the fields we save from stripe to only what we are using at the moment. Adhere to the same json structure
+    limited_stripe_customer = full_stripe_customer.try(:slice, :id)
+    if limited_stripe_subscription = parse_limited_stripe_subscription(full_stripe_customer)
+      limited_stripe_subscription[:plan] = parse_limited_stripe_subscription_plan(full_stripe_customer)
       limited_stripe_customer[:subscriptions] = {
         data: [
           limited_stripe_subscription
         ]
       }
     end
-
-    super limited_stripe_customer
-    self.stripe_customer_id = limited_stripe_customer[:id]
+    limited_stripe_customer
   end
+
+  def parse_limited_stripe_subscription(full_stripe_customer)
+    GenericHelper.try_nested_value_for_key_path(
+      full_stripe_customer,
+      [:subscriptions, :data, 0]
+    ).try(:slice, :id, :quantity)
+  end
+
+  def parse_limited_stripe_subscription_plan(full_stripe_customer)
+    GenericHelper.try_nested_value_for_key_path(
+      full_stripe_customer,
+      [:subscriptions, :data, 0, :plan]
+    ).try(:slice, :id, :amount)
+  end
+
+  public
 
   def stripe_customer_id
     stripe_customer[:id] || self[:stripe_customer_id]
@@ -82,32 +93,47 @@ class Family < ActiveRecord::Base
   end
 
   def update_or_create_stripe_subscription_if_needed!(credit_card_token=nil)
-    patient_count = patients.count
     if !stripe_customer_id && credit_card_token
-      customer_params = {
-        email: primary_guardian.email,
-        source: credit_card_token,
-        plan: STRIPE_PLAN,
-        quantity: patient_count
-      }
-      customer_params = customer_params.except(:plan, :quantity) if exempted?
-      self.stripe_customer = Stripe::Customer.create(customer_params).to_hash
-      renew_membership
+      create_stripe_customer(credit_card_token)
     elsif credit_card_token
-      customer = Stripe::Customer.retrieve stripe_customer_id
-      customer.source = credit_card_token
-      customer.save
-      renew_membership
-      PaymentsMailer.new_payment_method self
+      update_stripe_customer_payment_method(credit_card_token)
     elsif stripe_subscription_id
-      subscription = Stripe::Customer.retrieve(stripe_customer_id).subscriptions.data.first
-      subscription.quantity = patient_count
-      subscription.save
-      # immediately pay the proration amount
-      Stripe::Invoice.create(customer: stripe_customer_id).pay
-      self.stripe_customer = Stripe::Customer.retrieve(stripe_customer_id).to_hash
-      PaymentsMailer.subscription_updated self
+      update_subscription_quantity
     end
     save!
+  end
+
+  private
+
+  def create_stripe_customer(credit_card_token)
+    customer_params = {
+      email: primary_guardian.email,
+      source: credit_card_token,
+      plan: STRIPE_PLAN,
+      quantity: patients.count
+    }
+    customer_params = customer_params.except(:plan, :quantity) if exempted?
+    self.stripe_customer = Stripe::Customer.create(customer_params).to_hash
+    renew_membership
+  end
+
+  def update_stripe_customer_payment_method(credit_card_token)
+    customer = Stripe::Customer.retrieve stripe_customer_id
+    customer.source = credit_card_token
+    customer.save
+    renew_membership
+    PaymentsMailer.new_payment_method self
+  end
+
+  def update_subscription_quantity
+    subscription = Stripe::Customer.retrieve(stripe_customer_id).subscriptions.data.first
+    subscription.quantity = patients.count
+    subscription.save
+    self.stripe_customer = Stripe::Customer.retrieve(stripe_customer_id).to_hash
+    PaymentsMailer.subscription_updated self
+  end
+
+  def set_up_conversation
+    Conversation.create(family_id: id, state: :closed) unless Conversation.find_by_family_id(id)
   end
 end
--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 describe Family, type: :model do
+  before do
+    Stripe.api_key="test_key"
+    StripeMock.start
+    StripeMock.create_test_helper.create_plan(STRIPE_PLAN_PARAMS_MOCK)
+  end
 
+  let!(:user){ create(:user, :member) }
+  let!(:patient){ create(:patient, family: user.family) }
+  let!(:second_patient){ create(:patient, family: user.family) }
+
+  describe ".stripe_customer=" do
+    it "has a stripe customer with limited fields" do
+      user.family.update_or_create_stripe_subscription_if_needed!
+      expect(user.family.stripe_customer).to eq(
+        {
+          "id"=>"test_cus_3",
+          "subscriptions"=>{
+            "data"=>[
+              {
+                "id"=>"test_su_4",
+                "quantity"=>2,
+                "plan"=>{
+                  "id"=>"com.leohealth.standard",
+                  "amount"=>2000
+                }
+              }
+            ]
+          }
+        }
+      )
+    end
+  end
 end
