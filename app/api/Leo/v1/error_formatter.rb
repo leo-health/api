5ac2ae64abaab0b36445737c594ab3e8f30aed79
afanslau <afanslau@gmail.com>
2016-06-20 15:46:55 -0400

    697 - adds debug_message only when not production

--- app/api/Leo/v1/error_formatter.rb
+++ app/api/Leo/v1/error_formatter.rb
@@ -3,13 +3,9 @@ module Leo
     class ErrorFormatter
       def self.call message, backtrace, options, env
         nested_message = message
-        unless message.respond_to?(:keys) && message[:user_message]
-          nested_message = {
-            debug_message: message,
-            user_message: "Something went wrong! Please contact us at support@leohealth.com if the problem persists",
-            error_code: 500
-          }
-        end
+        nested_message = {debug_message: message} unless message.respond_to?(:keys)
+        nested_message[:user_message] ||= "Something went wrong! Please contact us at support@leohealth.com if the problem persists"
+        nested_message[:debug_message] = nil if Rails.env.production?
         error_json = { status: 'error', message: nested_message}
         error_json[:backtrace] = backtrace unless Rails.env.production?
         error_json.to_json
