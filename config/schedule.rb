7e575e86a37ca3475013cc4524d69c30032a70e5
Wuang Qin <wuang@leohealth.com>
2016-06-27 13:16:53 -0400

    delete five day appointment notification [branch ch97]

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,6 @@
 #set :output, {:error => 'log/cron.log', :standard => 'log/cron.log'}	#Misha, obsolete
 
 every 1.day, :at => '0:00 am' do
-  rake 'notification:five_day_prior_appointment'
   rake 'notification:one_day_prior_appointment'
   rake 'notification:patient_birthday'
   rake 'notification:account_confirmation_reminder'
