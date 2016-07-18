64fc64b3030c17c37a7ae52333b6d9d09672b5b4
Wuang Qin <wuang@leohealth.com>
2016-07-18 15:24:31 -0400

    add migration to drop enrollment table

---
+++ db/migrate/20160718191146_remove_enrollment_table.rb
@@ -0,0 +1,34 @@
+class RemoveEnrollmentTable < ActiveRecord::Migration
+  def up
+    drop_table :enrollments
+  end
+
+  def up
+    create_table :enrollments do |t|
+      t.string :title
+      t.string :first_name
+      t.string :middle_initial
+      t.string :last_name
+      t.string :suffix
+      t.string :sex
+      t.integer :practice_id
+      t.string :email
+      t.string :encrypted_password
+      t.integer :family_id
+      t.string :stripe_customer_id
+      t.integer :role_id
+      t.datetime :deleted_at
+      t.date :birth_date
+      t.string :avatar_url
+      t.integer :onboarding_group_id
+      t.string :authentication_token
+      t.boolean :invited_user, default: flase
+      t.integer :insurance_plan_id
+      t.string :phone
+      t.string :vendor_id
+      t.timestamps null: false
+    end
+    add_index :enrollments, :authentication_token
+    add_index :enrollments, :vendor_id, unique: true
+  end
+end
