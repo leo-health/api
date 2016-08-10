0530798704f3ae4edea0985c4bec35eb7d933aeb
Wuang Qin <wuang@leohealth.com>
2016-08-10 15:47:19 -0400

    add authorization check on messages

--- app/api/Leo/v1/conversations.rb
+++ app/api/Leo/v1/conversations.rb
@@ -42,7 +42,6 @@ module Leo
 
           put authorize: [:update, Conversation] do
             conversation = Conversation.find(params[:id])
-            authorize! :update, conversation
             escalated_to = User.find(params[:escalated_to_id])
             escalate_params = {escalated_to: escalated_to, note: params[:note], priority: params[:priority], escalated_by: current_user}
             if conversation.escalate!(escalate_params)
@@ -71,14 +70,14 @@ module Leo
           error!('403 Forbidden', 403) if current_user.guardian?
           conversations = params[:state] ? Conversation.where(state: params[:state]).order('updated_at desc') : Conversation.order("updated_at desc")
           max_page = (conversations.count / 10.to_f).ceil
-          authorize! :read, Conversation
           present :max_page, max_page
           present :conversations, paginate(Kaminari.paginate_array(conversations)), with: Leo::Entities::ShortConversationEntity
         end
 
         desc "Return a conversation by id"
-        get ':id', authorize: [:read, Conversation] do
+        get ':id' do
           conversation = Conversation.find(params[:id])
+          authorize! :read, conversation
           present :conversation, conversation, with: Leo::Entities::ShortConversationEntity
         end
       end
--- app/api/Leo/v1/messages.rb
+++ app/api/Leo/v1/messages.rb
@@ -1,98 +1,97 @@
 module Leo
   module V1
     class Messages < Grape::API
+      authorize_routes!
       include Grape::Kaminari
-      desc 'Return a single message'
-      namespace 'messages' do
+
+      resource :messages do
         before do
           authenticated
         end
 
+        desc 'Return a single message'
         get ':id' do
           message = Message.find(params[:id])
-          authorize! :read, Message
+          authorize! :read, message
           present message, with: Leo::Entities::MessageEntity, device_type: session_device_type
         end
       end
 
-      namespace 'conversations/:conversation_id' do
+      namespace 'conversations/:conversation_id/messages' do
         paginate per_page: MESSAGE_PAGE_SIZE
 
-        resource :messages do
-          before do
-            authenticated
-          end
+        before do
+          authenticated
+        end
 
-          params do
-            optional :start_datetime, type: DateTime, desc: "Start datetime", allow_blank: false
-            optional :end_datetime, type: DateTime, desc: "End datetime", allow_blank: false
-          end
+        after_validation do
+          @conversation = Conversation.find(params[:conversation_id])
+          authorize! :read, @conversation
+        end
 
-          after_validation do
-            @conversation = Conversation.find(params[:conversation_id])
-          end
+        params do
+          optional :start_datetime, type: DateTime, desc: "Start datetime", allow_blank: false
+          optional :end_datetime, type: DateTime, desc: "End datetime", allow_blank: false
+        end
 
-          namespace 'full' do
-            get do
-              messages = @conversation.messages
-              authorize! :read, Message
-              close_conversation_notes = @conversation.closure_notes
-              escalation_notes = @conversation.escalation_notes
-              full_messages =(messages + close_conversation_notes + escalation_notes).sort{ |x, y|y.created_at <=> x.created_at }
-              present :conversation_id, @conversation.id
-              present :init_message_id, @conversation.messages.first.id
-              present :messages, paginate(Kaminari.paginate_array(full_messages)), with: Leo::Entities::FullMessageEntity, device_type: session_device_type
-            end
-          end
+        desc "Return full object of messages"
+        get '/full' do
+          error!('403 Forbidden', 403) if current_user.guardian?
+          messages = @conversation.messages
+          close_conversation_notes = @conversation.closure_notes
+          escalation_notes = @conversation.escalation_notes
+          full_messages =(messages + close_conversation_notes + escalation_notes).sort{ |x, y|y.created_at <=> x.created_at }
+          present :conversation_id, @conversation.id
+          present :init_message_id, @conversation.messages.first.id
+          present :messages, paginate(Kaminari.paginate_array(full_messages)), with: Leo::Entities::FullMessageEntity, device_type: session_device_type
+        end
 
-          desc "Return all messages for a conversation with pagination options"
-          get do
-            messages = @conversation.messages.order('created_at DESC')
 
-            if params[:start_datetime]
-              start_datetime = Time.zone.parse(params[:start_datetime])
-              messages = messages.where("created_at > ?", start_datetime)
-              message_to_truncate = messages.last
-              if message_to_truncate
-                truncated_date = message_to_truncate.created_at.iso8601.to_datetime
-                if truncated_date <= start_datetime
-                  messages = messages.where.not id:messages.last.id
-                end                
+        desc "Return all messages for a conversation with pagination options"
+        get do
+          messages = @conversation.messages.order('created_at DESC')
+          if params[:start_datetime]
+            start_datetime = Time.zone.parse(params[:start_datetime])
+            messages = messages.where("created_at > ?", start_datetime)
+            message_to_truncate = messages.last
+            if message_to_truncate
+              truncated_date = message_to_truncate.created_at.iso8601.to_datetime
+              if truncated_date <= start_datetime
+                messages = messages.where.not id:messages.last.id
               end
             end
-
-            if params[:end_datetime]
-              end_datetime = Time.zone.parse(params[:end_datetime])
-              messages = messages.where("created_at < ?", end_datetime)
-            end
-
-            authorize! :read, Message
-            present paginate(messages), with: Leo::Entities::MessageEntity, device_type: session_device_type
           end
 
-          desc "Create a message"
-          params do
-            requires :body, type: String, allow_blank: false
-            requires :type_name, type: String, allow_blank: false, values: ['text', 'image']
+          if params[:end_datetime]
+            end_datetime = Time.zone.parse(params[:end_datetime])
+            messages = messages.where("created_at < ?", end_datetime)
           end
 
-          post do
-            message_params = { sender: current_user,
-                               type_name: params[:type_name] }
+          authorize! :read, Message
+          present paginate(messages), with: Leo::Entities::MessageEntity, device_type: session_device_type
+        end
 
-            if params[:type_name] == 'image'
-              message_params.merge!(message_photo_attributes: { image: image_decoder(params[:body]) })
-            else
-              message_params.merge!(body: params[:body])
-            end
-            message = @conversation.messages.new(message_params)
-            authorize! :create, message
-            if message.save
-              present message, with: Leo::Entities::MessageEntity, device_type: session_device_type
-              message.broadcast_message(current_user)
-            else
-              error!({ error_code: 422, user_message: message.errors.full_messages.first }, 422)
-            end
+        desc "Create a message"
+        params do
+          requires :body, type: String, allow_blank: false
+          requires :type_name, type: String, allow_blank: false, values: ['text', 'image']
+        end
+
+        post do
+          message_params = { sender: current_user, type_name: params[:type_name] }
+          if params[:type_name] == 'image'
+            message_params.merge!(message_photo_attributes: { image: image_decoder(params[:body]) })
+          else
+            message_params.merge!(body: params[:body])
+          end
+
+          message = @conversation.messages.new(message_params)
+          authorize! :create, message
+          if message.save
+            present message, with: Leo::Entities::MessageEntity, device_type: session_device_type
+            message.broadcast_message(current_user)
+          else
+            error!({ error_code: 422, user_message: message.errors.full_messages.first }, 422)
           end
         end
       end
