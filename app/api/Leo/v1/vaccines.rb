f6b10dbf7f12dda755675aae109c1fe737fcb39a
Wuang Qin <wuang@leohealth.com>
2016-08-31 14:06:22 -0400

    vaccines endpoint-revert point

--- app/api/Leo/v1/vaccines.rb
+++ app/api/Leo/v1/vaccines.rb
@@ -1,19 +1,24 @@
 module Leo
   module V1
     class Vaccines < Grape::API
-      resource :vaccines do
-        get do
-          content_type "application/octet-stream"
-          # TIMESTAMP_Jain_Leo_Flatiron Pediatrics_Immunization_History.pdf
-          header['Content-Disposition'] = "attachment; filename={}"
-          env['api.format'] = :binary
-          template = Tilt::ERBTemplate.new(Rails.root.join('app', 'views', 'vaccines', 'vaccines.html.erb'))
-          p = Patient.all[1]
-          output = template.render(p)
-          pdf = WickedPdf.new.pdf_from_string(output, {page_size: 'Letter'})
-          save_path = Rails.root.join('app', 'views', 'tmp.pdf')
-          File.open(save_path, 'wb'){|file| file << pdf }
+      before do
+        authenticated
+      end
+
+      # TIMESTAMP_Jain_Leo_Flatiron Pediatrics_Immunization_History.pdf
+      get ':patient_id/vaccines' do
+        content_type "application/octet-stream"
+        header['Content-Disposition'] = "attachment; filename=vaccines.pdf"
+        env['api.format'] = :binary
+        template = Tilt::ERBTemplate.new(Rails.root.join('app', 'views', 'vaccines', 'vaccines.html.erb'))
+        patient = Patient.find_by(id: params[:patient_id])
+        if patient
+          output = template.render(patient)
+          WickedPdf.new.pdf_from_string(output, {page_size: 'Letter'})
         end
+        # pdf = WickedPdf.new.pdf_from_string(output, {page_size: 'Letter'})
+        # save_path = Rails.root.join('app', 'views', 'tmp.pdf')
+        # File.open(save_path, 'wb'){|file| file << pdf }
       end
     end
   end
