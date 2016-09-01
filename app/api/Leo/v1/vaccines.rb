2dffa35353806cf4f772d2748c5fe54b0247d411
Wuang Qin <wuang@leohealth.com>
2016-09-01 15:57:39 -0400

    add default avatar

--- app/api/Leo/v1/vaccines.rb
+++ app/api/Leo/v1/vaccines.rb
@@ -10,9 +10,12 @@ module Leo
         env['api.format'] = :binary
         template = Tilt::ERBTemplate.new(Rails.root.join('app', 'views', 'vaccines', 'vaccines.html.erb'))
         patient = Patient.find_by(id: params[:patient_id])
-        if patient && patient.vaccines.count > 1
+        authorize! :read, patient
+        if patient && patient.vaccines.count > 0
           output = template.render(patient)
           WickedPdf.new.pdf_from_string(output, {page_size: 'Letter'})
+        else
+          error!({error_code: 422, user_message: "No vaccines record" }, 422)
         end
       end
     end
