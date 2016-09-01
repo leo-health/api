4b69c1a28ee219979d78388843edd2d092509112
Wuang Qin <wuang@leohealth.com>
2016-09-01 14:36:00 -0400

    add fix to erb template

--- app/api/Leo/v1/vaccines.rb
+++ app/api/Leo/v1/vaccines.rb
@@ -5,20 +5,15 @@ module Leo
         authenticated
       end
 
-      # TIMESTAMP_Jain_Leo_Flatiron Pediatrics_Immunization_History.pdf
       get ':patient_id/vaccines' do
-        content_type "application/octet-stream"
-        header['Content-Disposition'] = "attachment; filename=vaccines.pdf"
+        content_type "application/pdf"
         env['api.format'] = :binary
         template = Tilt::ERBTemplate.new(Rails.root.join('app', 'views', 'vaccines', 'vaccines.html.erb'))
         patient = Patient.find_by(id: params[:patient_id])
-        if patient
+        if patient && patient.vaccines.count > 1
           output = template.render(patient)
           WickedPdf.new.pdf_from_string(output, {page_size: 'Letter'})
         end
-        # pdf = WickedPdf.new.pdf_from_string(output, {page_size: 'Letter'})
-        # save_path = Rails.root.join('app', 'views', 'tmp.pdf')
-        # File.open(save_path, 'wb'){|file| file << pdf }
       end
     end
   end
