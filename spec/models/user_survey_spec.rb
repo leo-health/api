388fffb1966c78db8ac4920a3b2c89f837ff477f
Wuang Qin <wuang@leohealth.com>
2016-10-26 17:55:37 -0400

    add spec for generating survey --skip-ci

--- spec/models/user_survey_spec.rb
+++ spec/models/user_survey_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe UserSurvey, type: :model do
     it { is_expected.to validate_presence_of(:user) }
     it { is_expected.to validate_presence_of(:survey) }
   end
+
+  describe ".create_survey" do
+    let(:user){ create(:user) }
+    let!(:session){ user.sessions.create(
+      client_version: '1.5.1',
+      platform: 'ios',
+      device_token: 'token'
+    )}
+    let(:survey){ create(:survey) }
+    let(:patient){ create(:patient) }
+
+    it "should create a survey the specific patient and user" do
+       expect{ UserSurvey.create_survey(user, patient, survey) }.to change(UserSurvey, :count).by(1)
+    end
+
+    it "should create a notification for user" do
+      expect{ UserSurvey.create_survey(user, patient, survey) }.to change(Delayed::Job.where(queue: 'apns_notification'), :count).by(1)
+    end
+  end
 end
